5ac132f667e2703700af6d4fcc69d18e5c1296b2
Alex Penner <alex301p@googlemail.com>
2019-03-01 11:09:27 +0100

    Views laufen jetzt. First try for multi variable nodes doesnt work as intended :S

--- getting-started.jl
+++ getting-started.jl
@@ -33,7 +33,38 @@ ssl_prob= ODEProblem(ssl, x0 , (0., 2.))
 sol2 = solve(ssl_prob)
 plot(sol2, legend=false)
 
-sdl_prob = ODEProblem(sdl, v0, (0.,2.))
+sdl_prob = ODEProblem(sdl, v0, (0.,50.))
 sol3= solve(sdl_prob)
 #to separate lines and nodes, add something like vars=(1:10) in the plot function
 plot(sol3, legend=false,vars=(11:35))
+
+line! = (l,x_s,x_t,p,t) -> l .= x_s .- x_t
+lines! = [line! for e in edges(g)]
+dline! = (dl,l,x_s,x_t,p,t) -> dl .= x_s .- x_t .- l
+dlines! =[dline! for e in edges(g)]
+node! = (dx,x,l_s,l_t,p,t) -> dx .= x .- sum(sum.(l_s)) .+ sum(sum.(l_t))
+nodes! = [node! for n in vertices(g)]
+
+a= NetworkDynamics.static_line_network_dynamics(nodes!,lines!,g)
+b= NetworkDynamics.dynamic_line_network_dynamics(nodes!,dlines!,g)
+
+x0=rand(10)
+dx0=rand(10)
+v0=rand(35)
+
+a(dx0,x0,nothing,0.)
+b(v0,v0,nothing,0.)
+
+using DifferentialEquations
+
+a_prob=ODEProblem(a,x0,(0.,2.))
+sol=solve(a_prob)
+
+using Plots
+plot(sol,legend=false)
+
+b_prob=ODEProblem(b,v0,(0.,2.))
+sol2=solve(b_prob)
+plot(sol2,legend=false)
+
+zeros(10,5)
--- src/NetworkDynamics.jl
+++ src/NetworkDynamics.jl
@@ -8,9 +8,8 @@ export diffusive_network_dynamics
 
 """
     diffusive_network_dynamics(L, nodes)
-L: Matrix
-nodes: scalar function ``x \\rightarrow nodes(x)``
-"""
+    L: Matrix
+    nodes: scalar function ``x \\rightarrow nodes(x)``"""
 @with_kw struct diffusive_network_dynamics{T}
     L::AbstractArray{T,2}
     nodes::Function
@@ -19,13 +18,12 @@ end
 """
     (dnd::diffusive_network_dynamics)(dx, x, p, t)
 
-Calling a struct of type diffusive network dynamics implements the ODE:
+    Calling a struct of type diffusive network dynamics implements the ODE:
 
-``\\frac{dx_i}{dt} = nodes(x_i) + \\sum_j L_{ij} x_j``
+        ``\\frac{dx_i}{dt} = nodes(x_i) + \\sum_j L_{ij} x_j``
 
     dnd = diffusive_network_dynamics(L, nodes)
-    dnd(dx, x, p, t)
-"""
+    dnd(dx, x, p, t)"""
 
 function (dnd::diffusive_network_dynamics)(dx, x, p, t)
     mul!(dx, dnd.L, x) # dx .= L * x
@@ -38,105 +36,47 @@ function (dnd::diffusive_network_dynamics)(dx, x, p, t)
 end
 
 """
-When called with a graph, the dynamics defaults to using the laplacian.
+    When called with a graph, the dynamics defaults to using the laplacian.
 """
 function diffusive_network_dynamics(g::AbstractGraph, nodes)
     diffusive_network_dynamics(laplacian_matrix(g), nodes)
 end
 
-# The main, fully flexible network dynamics implementation
-export network_dynamics
+# The main, fully flexible network dynamics implementation.
 
 """
-The key functions or function arrays are:
+    The key functions or function arrays are:
 
-nodes: ``nodes!(dx, x, [l]_s,[l]_t, p, t)``
+    nodes: ``nodes!(dx, x, [l]_s,[l]_t, p, t)``
 
-lines: ``lines!(dl, l, x_s, x_t, p, t)``
+    lines: ``lines!(dl, l, x_s, x_t, p, t)``
 
-Given edges ``e``, ans nodes ``n``, as well as an orientation encoded by
-the source function ``s(e)`` and the target function ``t(e)``
-this implements the system of ODEs:
+    Given edges ``e``, ans nodes ``n``, as well as an orientation encoded by
+    the source function ``s(e)`` and the target function ``t(e)``
+    this implements the system of ODEs:
 
-``\\frac{dx_n}{dt} = dx_n``
+    ``\\frac{dx_n}{dt} = dx_n``
 
-``\\frac{dl_e}{dt} = dl_e``
+    ``\\frac{dl_e}{dt} = dl_e``
 
-with ``dx`` and ``dl`` calculated by
+    with ``dx`` and ``dl`` calculated by
 
-``[l]_s = [l_e \\text{ if } s(e) = n]``
+    ``[l]_s = [l_e \\text{ if } s(e) = n]``
 
-``[l]_t = [l_e \\text{ if } t(e) = n]``
+    ``[l]_t = [l_e \\text{ if } t(e) = n]``
 
-``nodes![n](dx_n, x_n, [l]_s, [l]_t, p_n, t)``
+    ``nodes![n](dx_n, x_n, [l]_s, [l]_t, p_n, t)``
 
-``lines![e](dl_e, l_e, x_{s(e)}, x_{t(e)}, p_e, t)``
+    ``lines![e](dl_e, l_e, x_{s(e)}, x_{t(e)}, p_e, t)``
 
-Alternative design:
+    Alternative design:
 
-Something that relaxes to a diffusive network would for example be
-implemented by
+    Something that relaxes to a diffusive network would for example be
+    implemented by
 
-    lines = (dl, l, x_1, x_2) -> dl .= 1000. * ((x_1 - x_2) - l)
-    agg = (list_1, list_2) -> sum(list_1) - sum(list_2)
-"""
+        lines = (dl, l, x_1, x_2) -> dl .= 1000. * ((x_1 - x_2) - l)
+        agg = (list_1, list_2) -> sum(list_1) - sum(list_2)"""
 
-@with_kw struct network_dynamics{T}
-    B::AbstractArray{T,2}
-    nodes::Function
-    lines::Function
-end
-
-function (nd::network_dynamics)(dv, v, p, t)
-    dx_temp=0
-    dl_temp=0
-    dx=zeros(size(nd.B,1))
-    dl=zeros(size(nd.B,2))
-    for i in 1:length(dx)
-        dv[i]= nd.nodes(dx_temp,v[i],p,t) - sum(Float64[v[k+size(nd.B,1)] for k in 1:size(nd.B,2) if nd.B[i,k]==1])
-        + sum(Float64[v[k+size(nd.B,1)] for k in 1:size(nd.B,2) if nd.B[i,k]==-1])
-    end
-    for j in (length(dx)+1):(length(dx)+length(dl))
-        dv[j]=nd.lines(dl_temp,v[j],p,t)
-    end
-    nothing
-end
-
-function network_dynamics(g::AbstractGraph, nodes, lines)
-    network_dynamics(incidence_matrix(g, oriented=true), nodes, lines)
-end
-
-
-
-export network_dynamics_on_the_line
-
-"""
-Documentation!!
-"""
-@with_kw struct network_dynamics_on_the_line{T}
-    B::AbstractArray{T,2}
-    nodes::Function
-    lines::Function
-end
-
-
-function (onl::network_dynamics_on_the_line)(dx, x, p, t)
-    dx_temp=0
-    l=1
-    for i in 1:length(dx)
-        onl.nodes(dx_temp,x[i],p,t)
-        dx[i]= dx_temp - sum(Float64[onl.lines(l,x,p,t)[k] for k in 1:size(onl.B,2) if onl.B[i,k]==1])
-        + sum(Float64[onl.lines(l,x,p,t)[k] for k in 1:size(onl.B,2) if onl.B[i,k]==-1])
-    end
-    nothing
-end
-
-"""
-When called with a graph, the dynamics defaults to using the laplacian.
-"""
-function network_dynamics_on_the_line(g::AbstractGraph, nodes, lines)
-    network_dynamics_on_the_line(incidence_matrix(g, oriented=true), nodes, lines)
-end
 
 export static_line_network_dynamics
 
@@ -179,9 +119,6 @@ function static_line_network_dynamics(nodes!, lines!, s_e, t_e)
         view(l_e_int, i)
         for i in 1:len_l ]
 
-    # This list contains the indices of the variables belonging to the nodes
-    #vorher x_idxs=[i for i in 1:len_n] gab einen 2d array raus
-
     # Create an array of views into the lines that selects only the sources
     # for each node
     l_s = [
@@ -205,5 +142,132 @@ function static_line_network_dynamics(nodes!, lines!, g::AbstractGraph)
     static_line_network_dynamics(nodes!, lines!, s_e, t_e)
 end
 
+
+export dynamic_line_network_dynamics
+
+@with_kw struct dynamic_line_network_dynamics
+    nodes!
+    lines!
+    s_e
+    t_e
+    l_e
+    l_e_int
+    l_s
+    l_t
+    len_l
+    len_n
+end
+
+
+function (d::dynamic_line_network_dynamics)(dx, x, p, t)
+    for i in 1:d.len_l
+        d.lines![i](d.l_e[i],view(x,d.len_n+i), x[d.s_e[i]], x[d.t_e[i]], p, t)
+        d.lines![i](view(dx,d.len_n+i),view(x,d.len_n+i), x[d.s_e[i]], x[d.t_e[i]], p, t)
+    end
+    for i in 1:d.len_n
+        d.nodes![i](view(dx,i), view(x,i), d.l_s[i], d.l_t[i], p, t)
+    end
+    nothing
+end
+
+function dynamic_line_network_dynamics(nodes!, lines!, s_e, t_e)
+    len_l = length(lines!)
+    len_n = length(nodes!)
+
+    # Will get longer once we have more variables per line
+    l_e_int = rand(len_l)
+
+    # This will be views to more than one variable eventually
+    l_e = [
+        view(l_e_int, i)
+        for i in 1:len_l ]
+
+    # Create an array of views into the lines that selects only the sources
+    # for each node
+    l_s = [
+         [l_e[j] for j in 1:len_l if s_e[j] == i]
+        for i in 1:len_n ]
+    # Create an array of views into the lines that selects only the targets
+    # for each node
+    l_t = [
+        [l_e[j] for j in 1:len_l if t_e[j] == i]
+        for i in 1:len_n ]
+
+    dynamic_line_network_dynamics(nodes!, lines!, s_e, t_e, l_e, l_e_int, l_s, l_t, len_l, len_n)
+end
+
+"""
+    When called with a graph, we construct the source and target vectors."""
+
+function dynamic_line_network_dynamics(nodes!, lines!, g::AbstractGraph)
+    s_e = [src(e) for e in edges(g)]
+    t_e = [dst(e) for e in edges(g)]
+    dynamic_line_network_dynamics(nodes!, lines!, s_e, t_e)
+end
+
+
+export multi_static
+
+@with_kw struct multi_static
+    nodes!
+    lines!
+    s_e
+    t_e
+    l_e
+    l_e_int
+    l_s
+    l_t
+    len_l
+    len_n
+    len_n_var
+end
+
+
+function (d::multi_static)(dx, x, p, t)
+    for i in 1:d.len_l
+        for j in 1:d.len_n_var
+            d.lines![i,j](d.l_e[i,j], x[(d.s_e[i]-1)*d.len_n_var + j], x[(d.t_e[i]-1)*d.len_n_var + j], p, t)
+        end
+    end
+    for i in 1:d.len_n
+        for j in 1:d.len_n_var
+            d.nodes![i,j](view(dx,i*d.len_n_var + j-1), view(x,(i*d.len_n_var):(i*d.len_n_var+j-1)), d.l_s[i,j], d.l_t[i,j], p, t)
+        end
+    end
+    nothing
+end
+
+function multi_static(nodes!, lines!, s_e, t_e)
+    len_l = length(lines!)
+    len_n = length(nodes!)
+    len_n_var=length(nodes![1])
+
+    # Will get longer once we have more variables per line
+    l_e_int = rand(len_l,len_n_var)
+
+    # This will be views to more than one variable eventually
+    l_e = [
+        view(l_e_int, i)
+        for i in 1:len_l ]
+
+    # Create an array of views into the lines that selects only the sources
+    # for each node
+    l_s = [
+         [l_e[j] for j in 1:len_l if s_e[j] == i]
+        for i in 1:len_n ]
+    # Create an array of views into the lines that selects only the targets
+    # for each node
+    l_t = [
+        [l_e[j] for j in 1:len_l if t_e[j] == i]
+        for i in 1:len_n ]
+
+    multi_static(nodes!, lines!, s_e, t_e, l_e, l_e_int, l_s, l_t, len_l, len_n,len_n_var)
+end
+
+function multi_static(nodes!, lines!, g::AbstractGraph)
+    s_e = [src(e) for e in edges(g)]
+    t_e = [dst(e) for e in edges(g)]
+    multi_static(nodes!, lines!, s_e, t_e)
+end
 
 end # module
