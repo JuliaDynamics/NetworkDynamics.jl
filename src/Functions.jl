d6c48fc5941f6461464e2954efaa35c1d945b446
Frank Hellmann <hellmann@pik-potsdam.de>
2019-04-12 14:25:18 +0200

    Keywords added to Functions

--- src/Functions.jl
+++ src/Functions.jl
@@ -22,64 +22,51 @@ export DDEEdge
 constructor network_dynamics. Shouldn't be too hard though. More of a design question. StaticVertex
 not implemented yet. =#
 
-
-struct StaticVertex #???
-    f! # ToDo
+@Base.kwdef struct StaticVertex
+    f! # (v, e_s, e_t, p, t) -> nothing
     dim # number of dimensions of x
-    sym # Symbols for the dimensions
+    sym=[:v for i in 1:dim] # Symbols for the dimensions
 end
 
-struct StaticEdge
-    f! # (l, v_s, v_t, p, t) -> nothing
+
+@Base.kwdef struct StaticEdge
+    f! # (e, v_s, v_t, p, t) -> nothing
     dim # number of dimensions of x
+    sym=[:e for i in 1:dim] # Symbols for the dimensions
 end
 
-struct ODEVertex
+
+@Base.kwdef struct ODEVertex
     f! # The function with signature (dx, x, e_s, e_t, p, t) -> nothing
     dim # number of dimensions of x
-    massmatrix # Mass matrix for the equation
-    sym # Symbols for the dimensions
+    massmatrix=nothing # Mass matrix for the equation
+    sym=[:v for i in 1:dim] # Symbols for the dimensions
 end
 
-# Here it would maybe be better to use keyword arguments instead of an additional function.
 
-function ODEVertex(f!, dim)
-    ODEVertex(f!, dim, sparse(1.0I, dim, dim), [:v for i in 1:dim])
-end
-
-struct ODEEdge
+@Base.kwdef struct ODEEdge
     f! # The function with signature (dx, x, e_s, e_t, p, t) -> nothing
     dim # number of dimensions of x
-    massmatrix # Mass matrix for the equation
-    sym # Symbols for the dimensions
+    massmatrix=nothing # Mass matrix for the equation
+    sym=[:e for i in 1:dim] # Symbols for the dimensions
 end
 
-function ODEEdge(f!, dim)
-    ODEEdge(f!, dim, sparse(1.0I, dim, dim), [:e for i in 1:dim])
-end
 
-struct DDEVertex
+@Base.kwdef struct DDEVertex
     f! # The function with signature (dv, v, h_v, e_s, e_t, h_e_s, h_e_d, p, t) -> nothing where h is the history function
     dim # number of dimensions
-    massmatrix # Mass matrix for the equation
-    sym # Symbols for the dimensions
-    tau_s # Array of Delays for the incoming currents of different variables
-    tau_d # Array of Delays for the outgoing currents of different variables
+    massmatrix=nothing # Mass matrix for the equation
+    sym=[:v for i in 1:dim] # Symbols for the dimensions
+    tau_s=[] # Array of Delays for the incoming currents of different variables
+    tau_d=[] # Array of Delays for the outgoing currents of different variables
 end
 
-function DDEVertex(f!, dim, tau_s, tau_d)
-    DDEVertex(f!, dim, sparse(1.0I, dim, dim), [:v for i in 1:dim], tau_s, tau_d)
-end
 
-struct DDEEdge
+@Base.kwdef struct DDEEdge
     f! # The function with signature (de, e, h_e, v_s, v_d, h_s, h_d, p, t)
     dim # number of variables
-    massmatrix # Mass matrix for the equation
-    sym # Symbols for the variables
-end
-
-function DDEEdge(f!,dim)
-    DDEEdge(f!,dim,sparse(1.0I, dim, dim), [:e for i in 1:dim])
+    massmatrix=nothing # Mass matrix for the equation
+    sym=[:v for i in 1:dim] # Symbols for the dimensions
 end
 
 const VertexFunction = Union{ODEVertex, StaticVertex, DDEVertex}
