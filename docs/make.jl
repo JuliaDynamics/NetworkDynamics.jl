7837f4ec389ef5809a36542cceb7025620b4c748
lindnemi <michaellindner@posteo.de>
2019-11-18 13:23:15 +0100

    Trying to push something to trigger the workflow

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,7 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
+# Test
 deploydocs(
     repo = "github.com/FHell/NetworkDynamics.jl.git",
 )
