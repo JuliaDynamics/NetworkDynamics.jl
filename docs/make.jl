0cb60bee5322e3f84e8234bf8baffcc4f201804f
lindnemi <michaellindner@posteo.de>
2019-11-18 12:56:56 +0100

    Setting up deploydocs(): created project.toml, modified make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/FHell/NetworkDynamics.jl.git",
+)
