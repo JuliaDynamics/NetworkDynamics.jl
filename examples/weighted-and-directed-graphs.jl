c9f97c132a3f8585b30b035d43e56bd5dcac5c63
lindnemi <michaellindner@pik-potsdam.de>
2020-02-17 11:10:22 +0100

    adjusted example code

--- examples/weighted-and-directed-graphs.jl
+++ examples/weighted-and-directed-graphs.jl
@@ -2,5 +2,70 @@
 # corresponding jupyter notebook on our github repository.
 
 using DelimitedFiles
+using SimpleWeightedGraphs, LightGraphs
+using NetworkDynamics
+using OrdinaryDiffEq
+using Plots
 
-G = readdlm("git/NetworkDynamics.jl/examples/Norm_G_DTI.txt", ',', Float64, '\n');
+
+### Reading the weight matrix (a brain atlas, [N.Tzourio-Mazoyer, 2002])
+
+G = readdlm(joinpath(@__DIR__, "Norm_G_DTI.txt"), ',', Float64, '\n');
+
+
+### Constructing the graph
+
+g_weighted = SimpleWeightedDiGraph(G)
+
+# For later use we extract the edge.weight attributes
+# . is the broadcasting operator and gets the attribute :weight of every edge
+edge_weights = getfield.(collect(edges(g_weighted)), :weight);
+
+
+### Setting up network_dynamics
+
+```
+Fitz-Hugh Nagumo vertex with electrical gap junctions
+```
+@inline Base.@propagate_inbounds function fhn_electrical_vertex!(dv, v, e_s, e_d, p, t)
+    dv[1] = v[1] - v[1]^3 / 3 - v[2]
+    dv[2] = (v[1] - a) * ϵ
+    for e in e_s
+        dv[1] -= e[1]
+    end
+    for e in e_d
+        dv[1] += e[1]
+    end
+    nothing
+end
+@inline Base.@propagate_inbounds function electrical_edge!(e, v_s, v_d, p, t)
+    e[1] =  p * (v_s[1] - v_d[1]) # * σ
+    nothing
+end
+
+electricaledge = StaticEdge(f! = electrical_edge!, dim = 1)
+odeelevertex = ODEVertex(f! = fhn_electrical_vertex!, dim = 2, sym=[:u, :v])
+
+fhn_network! = network_dynamics(odeelevertex, electricaledge, g_weighted)
+
+# Global parameters
+
+N = 90 # number of nodes in the brain atlas
+ϵ = 0.05
+a = .5
+σ = .5
+
+# When passing a tuple of parameters for nodes and edges, then p is passed indi-
+# vidually to every edge. For details see the docs on this example
+
+p = (nothing, σ * edge_weights)
+
+x0 = randn(2N) * 5
+tspan = (0., 200.)
+prob  = ODEProblem(fhn_network!, x0, tspan, p)
+sol   = solve(prob, AutoTsit5(TRBDF2()));
+
+
+### Plotting
+
+plot(sol, vars = idx_containing(fhn_network!, :u), legend = false, ylim=(-5, 5))
