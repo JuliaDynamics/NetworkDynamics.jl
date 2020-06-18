396bae7a26d0f4bb160945b7aed96869cc4b0063
lindnemi <michaellindner@pik-potsdam.de>
2020-06-18 17:46:28 +0200

    reworked example

--- examples/getting-started-with-DDEs.jl
+++ examples/getting-started-with-DDEs.jl
@@ -10,10 +10,10 @@ using Plots
 
 N = 20 # number of nodes
 k = 8  # average degree
-g = watts_strogatz(N, k, 0.) # a little more exciting than a bare random graph
+g = watts_strogatz(N, k, 0.) # ring
 
 # the signature of the edge and vertex functions differs from the ODE signature
-function diffusionedge!(e, v_s, v_d, h_v_s, h_v_d, p, t)
+function diffusionedge!(e, v_s, v_d, p, t)
     # usually e, v_s, v_d are arrays, hence we use the broadcasting operator .
     e .= .1 * (v_s - v_d)
     nothing
@@ -35,7 +35,10 @@ end
 
 ### Constructing the network dynamics
 
-# New Vertex type DDEVertex - DDEEdge might be part of a future release
+# New types: DDEVertex and StaticDelayEdge that both have access to the VERTEX history
+# DDEVertex is expected to have call signature (dv, v, e_s, e_d, h_v, p, t)
+# StaticDelayEdge is expected to have  signature (e, v_s, v_d, h_v_s, h_v_d, p, t)
+# StaticEdges get promoted to StaticDelayEdges [then their signature changes]
 nd_diffusion_vertex = DDEVertex(f! = diffusionvertex!, dim = 1)
 nd_diffusion_edge = StaticEdge(f! = diffusionedge!, dim = 1)
 
@@ -48,7 +51,7 @@ x0 = randn(N) # random initial conditions
 h(out, p, t) = (out .= 1.)
 tspan = (0., 10.)
 
-# i extended the tuple syntax to pass the delay τ as a parameter
+# i extended the tuple syntax to pass the delay time τ as a parameter
 # the first argument should be an array (or other object) containing the vertex parameters
 # the second argument holds the edge parameters and the third specifies the delay time τ
 # p = (vertexparameters, edgeparameters, delaytime)
@@ -67,7 +70,7 @@ plot(sol, vars = syms_containing(nd, "v"), legend=false)
 
 # We will have two independent diffusions on the network, hence dim = 2
 nd_diffusion_vertex_2 = DDEVertex(f! = diffusionvertex!, dim = 2, sym = [:x, :ϕ])
-nd_diffusion_edge_2 = StaticEdge(f! = diffusionedge!, dim = 2)
+nd_diffusion_edge_2 = StaticDelayEdge(f! = diffusionedge!, dim = 2)
 nd_2 = network_dynamics(nd_diffusion_vertex_2, nd_diffusion_edge_2, g)
 
 # at the moment there are issues with higher dimensional arrays in the DDE solve
@@ -85,33 +88,28 @@ sol_2 = solve(dde_prob_2, MethodOfSteps(Tsit5()));
 
 plot(sol_2, legend=false)
 
-using BenchmarkTools
-
-@btime nd($x0_2, $x0_2, $h, 0., 0.)
-
 ### Kuramoto model
 
 function kuramoto_delay_edge!(e, v_s, v_d, h_v_s, h_v_d, p, t)
-    # usually e, v_s, v_d are arrays, hence we use the broadcasting operator .
-    @. e = p * sin(h_v_s - v_d)
+    # The coupling is no longer symmetric, so we need to store BOTH values (see tutorials for details)
+    e[1] = p * sin(v_s[1] - h_v_d[1])
+    e[2] = p * sin(h_v_s[1] - v_d[1])
     nothing
 end
 
 function kuramoto_delay_vertex!(dv, v, e_s, e_d, h_v, p, t)
-    # usually v, e_s, e_d are arrays, hence we use the broadcasting operator .
-    dv .= p
-    # edges for which v is the source
+    dv[1] = p
     for e in e_s
-        dv .-= e
+        dv[1] -= e[1]
     end
-    # edges for which v is the destination
     for e in e_d
-        dv .+= e
+        dv[1] -= e[2]
     end
     nothing
 end
 
-kdedge! = StaticEdge(f! = kuramoto_delay_edge!, dim=1)
+# dim of the edge is 2 since the coupling is not symmetric
+kdedge! = StaticDelayEdge(f! = kuramoto_delay_edge!, dim=2)
 kdvertex! = DDEVertex(f! = kuramoto_delay_vertex!, dim = 1)
 
 nd! = network_dynamics(kdvertex!, kdedge!, g)
@@ -120,7 +118,9 @@ x0 = randn(N) # random initial conditions
 # history function defaults to all 1. and is in-place to save allocations
 h(out, p, t) = (out .= 1.)
 # p = (vertexparameters, edgeparameters, delaytime)
-p = (randn(N), 10., pi/2)
+ω = randn(N)
+ω .-= sum(ω)/N
+p = (ω, 2., 1.)
 tspan = (0.,20.)
 dde_prob = DDEProblem(nd!, x0, h, tspan, p)
 
@@ -129,5 +129,3 @@ sol = solve(dde_prob, MethodOfSteps(Tsit5()));
 ### Plotting
 
 plot(sol, vars = syms_containing(nd, "v"), legend=false)
-
-@btime nd!($x0, $x0, $h, p, 0.)
