485267dcb7d3e0ac6ff7b04d01a52ec283b60b4a
Anton Plietzsch <plietzsch@pik-potsdam.de>
2020-07-24 12:25:55 +0200

    added parameter handling to fixpoint search

--- examples/fixpoint.jl
+++ examples/fixpoint.jl
@@ -4,13 +4,14 @@ using NLsolve
 using LightGraphs
 using Plots
 using GraphPlot
+using Revise
 
 g = star_graph(4)
 gplot(g)
 
-function swing_eq!(dv, v, e_s, e_d, p, t)
+function swing_eq!(dv, v, e_s, e_d, P, t)
     dv[1] = v[2]
-    dv[2] = 1.0 - 0.1 * v[2]
+    dv[2] = P - 0.1 * v[2]
     for e in e_s
         dv[2] -= e[1]
     end
@@ -21,15 +22,15 @@ end
 
 swing_vertex = ODEVertex(f! = swing_eq!, dim = 2, sym=[:θ, :ω])
 
-function load_eq!(dv, v, e_s, e_d, p, t)
-    dv[1] = -1.0
+function load_eq!(dv, v, e_s, e_d, P, t)
+    dv[1] = P
     oriented_edge_sum!(dv, e_s, e_d)
 end
 
 load_vertex    = ODEVertex(f! = load_eq!, dim = 1, mass_matrix = 0, sym=[:θ])
 
-function powerflow_eq!(e, v_s, v_d, p, t)
-    e[1] = 6 * sin(v_s[1] - v_d[1])
+function powerflow_eq!(e, v_s, v_d, K, t)
+    e[1] = K * sin(v_s[1] - v_d[1])
 end
 
 powerflow_edge = StaticEdge(f! = powerflow_eq!, dim = 1)
@@ -38,8 +39,12 @@ vertex_array = [swing_vertex, swing_vertex, load_vertex, load_vertex]
 edge_array = [powerflow_edge for e in edges(g)]
 nd = network_dynamics(vertex_array, edge_array, g)
 
-u0 = find_fixpoint(nd, zeros(6))
+K = 6.0
+P = [1.0, 1.0, -1.0, -1.0]
+p = (P,K)
+
+u0 = find_fixpoint(nd, p, zeros(6))
 tspan = (0., 100.)
-ode_prob = ODEProblem(nd, u0, tspan, nothing)
+ode_prob = ODEProblem(nd, u0, tspan, p)
 ode_sol = solve(ode_prob, Rosenbrock23())
 plot(ode_sol, vars = syms_containing(nd, "θ"), legend = false)
