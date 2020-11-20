f44340b35be983ebfbaf90a4aaf4601905d7798d
lindnemi <michaellindner@pik-potsdam.de>
2020-11-20 13:19:33 +0100

    debug mode

--- test/time_and_test_refactor.jl
+++ test/time_and_test_refactor.jl
@@ -1,3 +1,5 @@
+using Pkg
+Pkg.activate(".")
 using BenchmarkTools
 using NetworkDynamics
 using Test
@@ -8,7 +10,7 @@ using Random
 for N = [1,10]
     # Pure Julia "ground truth"
 
-    g = watts_strogatz(N, 5, 0.)
+    g = watts_strogatz(N, Int(round(N/2)), 0.)
     println("\nTesting ND with $N nodes and $(ne(g)) edges:\n")
 
     B = incidence_matrix(g, oriented=true)
@@ -31,7 +33,7 @@ for N = [1,10]
     # NetworkDynamics
 
     function kuramoto_edge!(e, θ_s, θ_d, K, t)
-        e[1] = K * sin(θ_d[1] - θ_s[1])
+        e[1] = K * sin(θ_s[1] - θ_d[1])
     end
 
     function kuramoto_inertia!(dv, v, e_s, e_d, ω, t)
@@ -55,7 +57,7 @@ for N = [1,10]
     # Testing accuracy
 
     @testset "Accuracy" begin
-        for i in 1:30
+        for i in 1:1
             x0_θ = randn(N)
             x0_ω = randn(N)
 
@@ -90,3 +92,83 @@ end
 
 
 # For N = [20,200] ND#master is at 1.3 μs, 10.5 μs (median time) on my machine
+
+
+N = 2
+
+# Pure Julia "ground truth"
+
+g = watts_strogatz(N, Int(round(N/2)), 0.)
+println("\nTesting ND with $N nodes and $(ne(g)) edges:\n")
+
+
+g=SimpleGraph(N,1)
+B = incidence_matrix(g, oriented=true)
+
+struct kuramoto_dyn{T, T2, U}
+    B::T
+    B_trans::T2
+    ω::U
+    N::Int
+end
+function (dd::kuramoto_dyn)(dx, x, p, t)
+    dx[1:N] .= x[N+1:2N]
+    dx[N+1:2N] .= dd.ω .- x[N+1:2N] .- 5. * dd.B * sin.(dd.B_trans * x[1:N])
+    nothing
+end
+
+ω = Array{Float64}(1:N) ./ N
+kn! = kuramoto_dyn(B, transpose(B), ω, N)
+
+# NetworkDynamics
+
+function kuramoto_edge!(e, θ_s, θ_d, K, t)
+    e[1] = K * (θ_s[1] - θ_d[1])
+end
+
+function kuramoto_inertia!(dv, v, e_s, e_d, ω, t)
+    dv[1] = v[2]
+    dv[2] = ω - v[2]
+    #print(e_d)
+    for e in e_d
+        dv[2] += e[1]
+    end
+end
+
+inertia!  = ODEVertex(f! = kuramoto_inertia!, dim = 2, sym= [:θ, :ω]);
+edge!     = StaticEdge(f! = kuramoto_edge!, dim = 1)
+edge_anti! = StaticEdge(f! = kuramoto_edge!, dim = 1, coupling = :antisymmetric )
+
+
+
+nd! = network_dynamics(inertia!, edge!, g)
+nd_anti! = network_dynamics(inertia!, edge_anti!, g)
+nd_dir! = network_dynamics(inertia!, edge!, SimpleDiGraph(g))
+
+x0_θ = [1,2]
+x0_ω = [1,1]
+
+x0_kn = [x0_θ; x0_ω ]
+
+x0_nd = Vector(vec([x0_θ  x0_ω]'))
+x0_nd_anti = Vector(vec([x0_θ  x0_ω]'))
+x0_nd_dir = Vector(vec([x0_θ  x0_ω]'))
+
+dx_kn = zeros(2N)
+dx_nd = zeros(2N)
+dx_nd_anti = zeros(2N)
+dx_nd_dir = zeros(2N)
+
+kn!(dx_kn, x0_kn, nothing, 0.)
+@btime nd!(dx_nd, x0_nd, (ω, 5.), 0.)
+@btime nd_anti!(dx_nd_anti, x0_nd_anti, (ω, 5.), 0.)
+@btime nd_dir!(dx_nd_dir, x0_nd_dir, (ω, 5.), 0.)
+
+
+dx_nd
+dx_nd_anti
+dx_nd_dir
+
+
+
+dx_nd .- dx_nd_dir
