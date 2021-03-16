0a5950ef0bcbe05cb0f9b04d9e99f6995776108f
koulen <koulen@pik-potsdam.de>
2021-03-16 20:34:18 +0100

    changes in jac_structs and example

--- Jacobians/example_scetch.jl
+++ Jacobians/example_scetch.jl
@@ -1,22 +1,17 @@
-using NetworkDynamics
-using ForwardDiff
+# using NetworkDynamics
+
 using LightGraphs
+using Reexport
 
+include("/Users/lalalulu/Desktop/PIK/github/NetworkDynamics.jl/src/Utilities.jl")
+@reexport using .Utilities
 
-include()
+include("/Users/lalalulu/Desktop/PIK/github/NetworkDynamics.jl/Jacobians/jac_structs.jl")
+@reexport using .jac_structs
 
-N = 5
-k = 2
-g = barabasi_albert(N, k)
-x0 = [rand() for i in 1:N]
-tspan = (0., 10.)
-# parameters
-omegas = rand()*2π
-coupling_constant = 3.0/(N-1)
-p = (nothing, nothing)
+include("/Users/lalalulu/Desktop/PIK/github/NetworkDynamics.jl/Jacobians/jac_functions.jl")
+@reexport using .jac_functions
 
-t_max=10.
-edges1=0.
 
 function diffusionedge!(e, v_s, v_d, p, t)
     # usually e, v_s, v_d are arrays, hence we use the broadcasting operator .
@@ -26,36 +21,27 @@ end
 
 function diffusionvertex!(dv, v, edges, p, t)
     # usually dv, v, edges are arrays, hence we use the broadcasting operator .
-    dv = 0.
+    dv .= 0.
     for e in edges
         dv .+= e
     end
     nothing
 end
 
+nd_diffusion_vertex = ODEVertex(f! = diffusionvertex!, dim = 1)
+
+nd_diffusion_edge = StaticEdge(f! = diffusionedge!, dim = 1)
+
+nd = network_dynamics(nd_diffusion_vertex, nd_diffusion_edge, g)
+
+### Simulation
+
+x0 = randn(N) # random initial conditions
+ode_prob = ODEProblem(nd, x0, (0., 4.))
+sol = solve(ode_prob, Tsit5());
 
 function VertexJacobian!(J, v, p, t)
    J = internal_jacobian(v, p, t)
    # get_vertex_jacobian(gd, i) = something(v,p,t)
    # gd.v_jac_array[i] = something(v,p,t)
 end
-
-
-# signature of StaticVertex: (f!, dim, mass_matrix, sym, z, jac)
-nd_jac_vertex = StaticVertex(f! = diffusionvertex!, dim = 1)#, jac=true
-# signature of StaticEdge: (f!, dim, coupling, sym, jac)
-nd_jac_edge = StaticEdge(f! = diffusionedge!, dim = 1)#, jac=true)
-
-nd_jac = network_dynamics(nd_jac_vertex, nd_jac_edge, g, jac = true)
-
-
-
-nd_jac_operator = NDJacVecOperator(x0,p,tspan,g) #gs, GD,bool?
-
-for t in 1:tmax
-    nd_jac_operator(x0,p,t) ## Jac*z
-end
-
-nd_jac_operator(dx,x0,p,tspan) ## J mul!(dx,Jac,z)
-
-# jac = jacvecprod in source ordner
--- Jacobians/jac_structs.jl
+++ Jacobians/jac_structs.jl
@@ -98,7 +98,7 @@ For more details see the documentation.
                            dim::Int,
                            coupling::Symbol,
                            sym::Vector{Symbol},
-                           edge_jacobian!) where T # beliebiger Typ
+                           edge_jacobian!) where T
 
         coupling_types = (:undefined, :directed, :fiducial, :undirected, :symmetric,
                           :antisymmetric)
@@ -279,201 +279,8 @@ For more details see the documentation.
     end
 end
 
-######################## Delay Block ###########################################
-"""
-    DDEVertex(f!, dim, mass_matrix, sym)
-
-Wrapper that ensures compatibility of a **mutating** function **`f!`** with
-the key constructor `network_dynamics`.
-
-**`f!`**  describes the local behaviour at a dynamic node and has to respect
-the following calling syntax
-
-```julia
-f!(dv, v, edges, h, p, t) -> nothing
-```
-
-Here `dv`, `v`, `p` and `t` are the usual ODE arguments, while
-`edges` is an Arry of incoming edges. `h` is the history array for `v`.
-
-- `dim` is the number of independent variables in the edge equations and
-- `sym` is an array of symbols for these variables.
-- `coupling` is a Symbol describing if the EdgeFunction is intended for a directed graph (`:directed`) or for an undirected graph (`{:undirected, :fiducial}`). `:directed` is intended for directed graphs. `:undirected` is the default option and is only compatible with SimpleGraph. in this case f! should specify the coupling from a source vertex to a destination vertex.  `:fiducial` lets the user specify both the coupling from src to dst, as well as the coupling from dst to src and is intended for advanced users.
-- `mass_matrix` is an optional argument that defaults to the identity
-matrix `I`. If a mass matrix M is given the system `M * de = f!` will be
-solved.
-
-For more details see the documentation.
-"""
-@Base.kwdef struct DDEVertex{T} <: VertexFunction
-    f!::T # The function with signature (dx, x, edges, h, p, t) -> nothing
-    dim::Int # number of dimensions of x
-    mass_matrix=I # Mass matrix for the equation
-    sym=[:v for i in 1:dim] # Symbols for the dimensions
-end
-
-function DDEVertex(ov::ODEVertex)
-    let _f! = ov.f!, dim = ov.dim, sym = ov.sym, mass_matrix = ov.mass_matrix
-        f! = @inline (dv, v, dst_edges, h_v, p, t) -> _f!(dv, v, dst_edges, p, t)
-        return DDEVertex(f!, dim, mass_matrix, sym)
-    end
-end
-
-
-function DDEVertex(sv::StaticVertex)
-    let _f! = ODE_from_Static(sv.f!), dim = sv.dim, sym = sv.sym
-        f! = @inline (dv, v, dst_edges, h_v, p, t) -> _f!(dv, v, dst_edges, p, t)
-        return DDEVertex(f!, dim, 0., sym)
-    end
-end
-
-# Promotion rules [eventually there might be too many types to hardcode everyhting]
-
-convert(::Type{DDEVertex}, x::StaticVertex) = DDEVertex(x)
-promote_rule(::Type{DDEVertex}, ::Type{StaticVertex}) = DDEVertex
-
-
-convert(::Type{DDEVertex}, x::ODEVertex) = DDEVertex(x)
-promote_rule(::Type{DDEVertex}, ::Type{ODEVertex}) = DDEVertex
-
-
-"""
-Like a static edge but with extra arguments for the history of the source and destination vertices. This is NOT a DDEEdge.
-"""
-@Base.kwdef struct StaticDelayEdge{T} <: EdgeFunction
-    f!::T # (e, v_s, v_t, h_v_s, h_v_d, p, t) -> nothing
-    dim::Int # number of dimensions of e
-    coupling = :undefined # :directed, :symmetric, :antisymmetric, :fiducial, :undirected
-    sym=[:e for i in 1:dim] # Symbols for the dimensions
-
-
-    function StaticDelayEdge(user_f!::T,
-                           dim::Int,
-                           coupling::Symbol,
-                           sym::Vector{Symbol}) where T
-
-        coupling_types = (:undefined, :directed, :fiducial, :undirected, :symmetric,
-                          :antisymmetric)
-
-        coupling ∈ coupling_types ? nothing :
-            error("Coupling type not recognized. Choose from $coupling_types.")
-
-        dim > 0 ? nothing : error("dim has to be a positive number.")
-
-        dim == length(sym) ? nothing : error("Please specify a symbol for every dimension.")
-
-        if coupling ∈ [:undefined, :directed]
-            return new{T}(user_f!, dim, coupling, sym)
-
-        elseif coupling == :fiducial
-            dim % 2 == 0 ? nothing : error("Fiducial edges are required to have even dim.
-                                            The first dim args are used for src -> dst,
-                                            the second for dst -> src coupling.")
-            return new{T}(user_f!, dim, coupling, sym)
-
-        elseif coupling == :undirected
-            # This might cause unexpected behaviour if source and destination vertex don't
-            # have the same internal arguments.
-            # Make sure to explicitly define the edge is :fiducial in that case.
-            f! = @inline (e, v_s, v_d, h_v_s, h_v_d, p, t) -> begin
-                @inbounds user_f!(view(e,1:dim), v_s, v_d, h_v_s, h_v_d, p, t)
-                @inbounds user_f!(view(e,dim+1:2dim), v_d, v_s, h_v_d, h_v_s, p, t)
-                nothing
-            end
-        elseif coupling == :antisymmetric
-            f! = @inline (e, v_s, v_d, h_v_s, h_v_d, p, t) -> begin
-                @inbounds user_f!(view(e,1:dim), v_s, v_d, h_v_s, h_v_d, p, t)
-                @inbounds for i in 1:dim
-                    e[dim + i] = -1.0 * e[i]
-                end
-                nothing
-            end
-        elseif coupling == :symmetric
-            f! = @inline (e, v_s, v_d, h_v_s, h_v_d, p, t) -> begin
-                @inbounds user_f!(view(e,1:dim), v_s, v_d, h_v_s, h_v_d, p, t)
-                @inbounds for i in 1:dim
-                    e[dim + i] = e[i]
-                end
-                nothing
-            end
-        end
-        return new{typeof(f!)}(f!, 2dim, coupling, repeat(sym, 2))
-    end
-end
-
-function StaticDelayEdge(se::StaticEdge)
-    let _f! = se.f!, dim = se.dim, coupling = se.coupling, sym = se.sym
-        f! = @inline (e, v_s, v_d, h_v_s, h_v_d, p, t) -> _f!(e, v_s, v_d, p, t)
-        if coupling ∈ (:undefined, :fiducial, :directed)
-            return StaticDelayEdge(f!, dim, coupling, sym)
-        else
-            return StaticDelayEdge(f!, dim, :fiducial, sym)
-        end
-    end
-end
-
-# Promotion rules
-
-convert(::Type{StaticDelayEdge}, x::StaticEdge) = StaticDelayEdge(x)
-promote_rule(::Type{StaticDelayEdge}, ::Type{StaticEdge}) = StaticDelayEdge
-
-
-
-convert(::Type{ODEVertex}, x::StaticVertex) = ODEVertex(x)
-promote_rule(::Type{ODEVertex}, ::Type{StaticVertex}) = ODEVertex
-
-# Not sure if the next line does something?
-promote_rule(::Type{ODEVertex{T}}, ::Type{ODEVertex{U}}) where {T, U} = ODEVertex
-
-convert(::Type{ODEEdge}, x::StaticEdge) = ODEEdge(x)
-promote_rule(::Type{ODEEdge}, ::Type{StaticEdge}) = ODEEdge
-"""
-Promotes a StaticVertex to an ODEVertex.
-"""
-struct ODE_from_Static{T}
-    f!::T
-end
-function (ofs::ODE_from_Static)(dx,x,args...)
-    # If mass matrix = 0 the differential equation sets dx = 0.
-    # To set x to the value calculated by f! we first write the value calculated
-    # by f! into dx, then subtract x. This leads to the  constraint
-    # 0 = - x + f(...)
-    # where f(...) denotes the value that f!(a, ...) writes into a.
-    ofs.f!(dx, args...)
-    @inbounds for i in eachindex(dx)
-        dx[i] -= x[i]
-    end
-    nothing
-end
-
-function ODEVertex(sv::StaticVertex)
-    let _f! = sv.f!, dim = sv.dim, sym = sv.sym
-        return ODEVertex(ODE_from_Static(_f!), dim, 0., sym)
-    end
-end
-
-function ODEEdge(se::StaticEdge)
-    let _f! = se.f!, dim = se.dim, coupling = se.coupling, sym = se.sym
-        if coupling == :undirected
-            # undirected means the reconstruction has already happend and the edge may now
-            # be considered fiducial
-            return ODEEdge(ODE_from_Static(_f!), dim, :fiducial, 0., sym)
-        else
-            return ODEEdge(ODE_from_Static(_f!), dim, coupling, 0., sym)
-        end
-    end
-end
-
-end
-
-######################## Delay Block Ende ######################################
-
 ######################## Graph Data Block ######################################
-### TO DO: in NetworkStructures.jl
-# field names von graph data
 
-
-# Array{T,N}(undef, dims), N ist Dimension des arrays, T ist Typ des arrays
 struct EdgeData{GDB, elE} <: AbstractArray{elE, 1}
     gdb::GDB
     idx_offset::Int
@@ -504,13 +311,21 @@ struct GraphData{GDB, elV, elE}
     v_s_e::Array{VertexData{GDB, elV}, 1} # the vertex that is the source of e
     v_d_e::Array{VertexData{GDB, elV}, 1} # the vertex that is the destination of e
     dst_edges::Array{Array{EdgeData{GDB, elE}, 1}, 1} # the half-edges that have v as destination
+end
 
-
-### auslagern
-    v_jac_array::Array{Array{Float, 2}, 1} # Dimension richtig? da Jacobians (maximal) 2 dim. # oder elVJac
+struct JacGraphData{JGDB}
+    jgdb::JGDB
+    v_jac_array::Array{Array{Float, 2}, 1}
     e_jac_array::Array{Array{Array{Float, 2}, 1}, 1}
     e_jac_product::Array{Float, 2}
 end
+
+#=struct JacGraphData{JGDB, elV, elE, elEP}
+#    jgdb::JGDB
+#    v_jac_array::Array{VertexData{JGDB, elV}, 1}
+#    e_jac_array::Array{Array{EdgeData{JGDB, elE}, 1}, 1}
+#    e_jac_product::EdgeData{JGDB, elEP}
+end=#
 
 function GraphData(v_array::Tv, e_array::Te, gs::GraphStruct; global_offset = 0) where {Tv, Te}
     gdb = GraphDataBuffer{Tv, Te}(v_array, e_array)
@@ -523,18 +338,35 @@ function GraphData(v_array::Tv, e_array::Te, gs::GraphStruct; global_offset = 0)
     v_d_e = [VertexData{GDB, elV}(gdb, offset + global_offset, dim) for (offset,dim) in zip(gs.d_e_offs, gs.v_dims[gs.d_e])]
     dst_edges = [[EdgeData{GDB, elE}(gdb, offset + global_offset, dim) for (offset,dim) in in_edge] for in_edge in gs.dst_edges_dat]
     GraphData{GDB, elV, elE}(gdb, v, e, v_s_e, v_d_e, dst_edges)
+end
 
-### auslagern
+function JacGraphData(v_Jac_array::Tvj, e_Jac_array::Tej, e_Jac_product_array::Tep, gs::GraphStruct) where {Tvj, Tej}
+    jgdb = JacGraphDataBuffer{Tvj, Tej, Tep}(v_Jac_array, e_Jac_array, e_Jac_product_array)
+
+    v_jac = [Array{Float64,2}(undef, dim, dim) for dim in gs.v_dims]
+    e_jac = [[zeros(dim, srcdim), zeros(dim, dstdim)] for (dim, srcdim, dstdim) in zip(gs.e_dims, gs.v_dims, gs.v_dims)] # homogene Netzwerke: v_src_dim = v_dst_dim = v_dim
+    e_jac_product =  zeros(gs.e_dims[1], gs.num_e) # Annahme: homogene edges
+
+    JacGraphData{JGDB}(jgdb, v_jac, e_jac, e_jac_product)
+end
+
+
+#=function JacGraphData(v_Jac_array::Tvj, e_Jac_array::Tej, e_Jac_product_array::Tep, gs::GraphStruct) where {Tvj, Tej}
+    jgdb = JacGraphDataBuffer{Tvj, Tej, Tep}(v_Jac_array, e_Jac_array, e_Jac_product_array)
+    JGDB = typeof(jgdb)
+    elV = eltype(v_jac_array)
+    elE = eltype(e_Jac_array)
+    elEP = eltype(e_Jac_product_array)
+
     v_jac_array = [Array{Float64,2}(undef, dim, dim) for dim in gs.v_dims]
-    # For edges there is another array layer in the buffer since each edge has two Jacobians
-    #e_jac_array = [[zeros(dim, srcdim), zeros(dim, dstdim)] for (dim, srcdim, dstdim) in zip(gs.e_dims, v_src_dims, v_dst_dims)]
     e_jac_array = [[zeros(dim, srcdim), zeros(dim, dstdim)] for (dim, srcdim, dstdim) in zip(gs.e_dims, gs.v_dims, gs.v_dims)] # homogene Netzwerke: v_src_dim = v_dst_dim = v_dim
     e_jac_product =  zeros(gs.e_dims[1], gs.num_e) # Annahme: homogene edges
-end
+
+    JacGraphData{JGDB}(jgdb, v_jac_array, e_jac_array, e_jac_product)
+end =#
 
 ### TO DO: in NetworkStructures.jl
 
-#@inline function get_src_edge_jacobian(gd::GraphData, i::Int) = gd.e_jac_array[i, 1] ## Micha: gd.e_jac_array[i][1]
 @inline function get_src_edge_jacobian(gd::JacGraphData, i::Int) = gd.e_jac_array[i][1]
 
 @inline function get_dst_edge_jacobian(gd::JacGraphData, i::Int) = gd.e_jac_array[i][2]
@@ -544,23 +376,13 @@ end
 
 ############################ NDJacVecOperator ##################################
 mutable struct NDJacVecOperator{uType, P, tType, G, GD} <: DiffEqBase.AbstractDiffEqLinearOperator{T} # mutable da x, p, t geupdated werden
-    x::uType # Punkt u, für welchen das jvp berechnet wird
+    x::uType
     p::P
     t::tType
     graph::G
     graph_structure::GraphStruct
     graph_data::GD
-    parallel::Bool
-end
-
-## solle wir wie chris eine mutable struct machen?
-mutable struct NDJacVecOperator{uType, P, tType,G,GraphStruct,GD,Bool}# <: DiffEqBase.AbstractDiffEqLinearOperator{T}
-    x::uType # Punkt u, für welchen das jvp berechnet wird
-    p::P
-    t::tType
-    graph::G
-    graph_structure::GraphStruct
-    graph_data::GD
+    # jac_graph_data::JGD - dann muss oben noch JGD hin
     parallel::Bool
 end
 
@@ -598,17 +420,21 @@ function network_dynamics(vertices!::Union{Array{T, 1}, T},
 
     if jac == true
         # These additional arrays are used for initializing the GraphData and will be overwritten
-        # v_jac_array = similar(x_prototype, sum(v_dims))
-        # e_jac_array = similar(x_prototype, sum(e_dims))
-        # e_jac_product = similar(x_prototype, sum(num_e))
+        v_Jac_array = similar(x_prototype, sum(v_dims))
+        e_Jac_array = similar(x_prototype, sum(e_dims))
+        e_Jac_product_array = similar(x_prototype, sum(num_e))
 
-        graph_stucture = GraphStruct(graph, v_dims, e_dims, symbols_v, symbols_e)
+        graph_stucture = GraphStruct(graph, v_dims, e_dims, symbols_v, symbols_e) # Funktion
 
-        graph_data = GraphData(v_array, e_array, graph_stucture, v_jac_array, e_jac_array, e_jac_product)
+        graph_data = GraphData(v_array, e_array, graph_stucture) # Funktion
 
-        nd! = nd_ODE_Static(vertices!, edges!, graph, graph_stucture, graph_data, parallel)
+        jac_graph_data = JacGraphData(v_jac_array, e_jac_array, e_jac_product, graph_structure) # Funktion
+
+        nd! = nd_ODE_Static(vertices!, edges!, graph, graph_stucture, graph_data, parallel) # Objekterstellung, nd_ODE_Static hier struct
+
         mass_matrix = construct_mass_matrix(mmv_array, graph_stucture)
-        nd_jac_operator = NDJacVecOperator
+
+        nd_jac_operator = NDJacVecOperator(x, p, t, graph, graph_structure, graph_data, jac_graph_data, parallel) # Problem: x, p, t nicht da
 
         ODEFunction(nd!; mass_matrix = mass_matrix, jac = nd_jac_operator, syms = symbols)
     end
