23fefbf5d83d943ea285ff8dd6c7c1e275e1dfdd
koulen <koulen@pik-potsdam.de>
2021-03-30 09:56:51 +0200

    testing update_coefficients

--- Jacobians/jac_functions.jl
+++ Jacobians/jac_functions.jl
@@ -4,10 +4,14 @@ using LinearAlgebra
 using NetworkDynamics
 using LightGraphs
 using DiffEqBase
-using DiffEqOperators
+import DiffEqBase.update_coefficients!
 
-#include("/Users/lalalulu/Desktop/PIK/github/NetworkDynamics.jl/Jacobians/jac_structs.jl")
-#@reexport using .jac_structs
+
+include("/Users/lalalulu/Desktop/PIK/github/NetworkDynamics.jl/Jacobians/jac_structs.jl")
+@reexport using .jac_structs
+
+#include("/Users/lalalulu/Desktop/PIK/github/NetworkDynamics.jl/src/nd_ODE_Static.jl")
+#@reexport using .nd_ODE_Static_mod
 
 N = 4
 k = 2
@@ -43,10 +47,13 @@ sol = solve(ode_prob, Tsit5());
 
 # collecting the graph infos
 
-nd.f
 graph_structure_ = nd.f.graph_structure
 graph_data_ = nd.f.graph_data
-graph_ = nd.f.graph
+#graph_ = nd.f.graph
+
+v_dims = nd.f.graph_structure.v_dims
+e_dims = nd.f.graph_structure.e_dims
+num_e = nd.f.graph_structure.num_e
 
 # build the jacobian graph data
 
@@ -56,91 +63,20 @@ struct JacGraphData
     e_jac_product::Array{Float64, 2}
 end
 
-v_dims = nd.f.graph_structure.v_dims
-e_dims = nd.f.graph_structure.e_dims
-num_e = nd.f.graph_structure.num_e
-
 v_jac_array = [Array{Float64,2}(undef, dim, dim) for dim in v_dims]
 e_jac_array = [[zeros(dim, srcdim), zeros(dim, dstdim)] for (dim, srcdim, dstdim) in zip(e_dims, v_dims, v_dims)] # homogene Netzwerke: v_src_dim = v_dst_dim = v_dim
 e_jac_product =  zeros(e_dims[1], num_e) # Annahme: homogene edges
 
 jac_graph_data_object = JacGraphData(v_jac_array, e_jac_array, e_jac_product)
 
-mutable struct NDJacVecOperator{uType, P, tType, G, GD, JGD, T} <: DiffEqBase.AbstractDiffEqLinearOperator{T} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    graph::G
-    graph_structure::GraphStruct
-    graph_data::GD
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
 x = similar(zeros(1), sum(v_dims))
 p = nothing
 t = 0.0
 parallel = false
 
-NDJacVecOperator_object = NDJacVecOperator(x, p, t, g, graph_structure_, graph_data_, jac_graph_data_object, parallel)
+#### NDJacVecOperator
 
-
-#### modified mutable struct
-
-mutable struct NDJacVecOperator6{uType, tType, G, GS, GD, JGD} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    graph::G
-    graph_structure::GS
-    graph_data::GD
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator6_object = NDJacVecOperator6(x, p, t, g, graph_structure_, graph_data_, jac_graph_data_object, parallel)
-
-
-mutable struct NDJacVecOperator7{uType, tType, G, GS, GD, JGD, T} <: DiffEqBase.AbstractDiffEqLinearOperator{T} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    graph::G
-    graph_structure::GS
-    graph_data::GD
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator7_object = NDJacVecOperator7(x, p, t, g, graph_structure_, graph_data_, jac_graph_data_object, parallel)
-
-mutable struct NDJacVecOperator8{T, uType, tType, G, GS, GD, JGD} <: DiffEqBase.AbstractDiffEqLinearOperator{T} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    graph::G
-    graph_structure::GS
-    graph_data::GD
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator8_object = NDJacVecOperator8(x, p, t, g, graph_structure_, graph_data_, jac_graph_data_object, parallel)
-
-mutable struct NDJacVecOperator9{T, uType, tType, G, GD, JGD} <: DiffEqBase.AbstractDiffEqLinearOperator{T} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    graph::G
-    graph_structure::GraphStruct
-    graph_data::GD
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator9_object = NDJacVecOperator9(x, p, t, g, graph_structure_, graph_data_, jac_graph_data_object, parallel)
-
-mutable struct NDJacVecOperator10{T, uType, tType, G, GD, JGD} <: DiffEqBase.AbstractDiffEqLinearOperator{T} # mutable da x, p, t geupdated werden
+mutable struct NDJacVecOperator{T, uType, tType, G, GD, JGD} <: DiffEqBase.AbstractDiffEqLinearOperator{T} # mutable da x, p, t geupdated werden
     x::uType
     p
     t::tType
@@ -150,16 +86,16 @@ mutable struct NDJacVecOperator10{T, uType, tType, G, GD, JGD} <: DiffEqBase.Abs
     jac_graph_data::JGD
     parallel::Bool
 
-    function NDJacVecOperator10{T}(x, p, t, graph, graph_structure, graph_data, jac_graph_data, parallel) where T
+    function NDJacVecOperator{T}(x, p, t, graph, graph_structure, graph_data, jac_graph_data, parallel) where T
         new{T,typeof(x),typeof(t),typeof(graph),typeof(graph_data),typeof(jac_graph_data)}(x, p, t, graph, graph_structure, graph_data, jac_graph_data, parallel)
     end
 
-    function NDJacVecOperator10(x, p, t, graph, graph_structure, graph_data, jac_graph_data, parallel)
-        NDJacVecOperator10{eltype(x)}(x, p, t, graph, graph_structure, graph_data, jac_graph_data, parallel)
+    function NDJacVecOperator(x, p, t, graph, graph_structure, graph_data, jac_graph_data, parallel)
+        NDJacVecOperator{eltype(x)}(x, p, t, graph, graph_structure, graph_data, jac_graph_data, parallel)
     end
 end
 
-NDJacVecOperator10_object = NDJacVecOperator10(x, p, t, g, graph_structure_, graph_data_, jac_graph_data_object, parallel)
+NDJacVecOperator_object = NDJacVecOperator(x, p, t, g, graph_structure_, graph_data_, jac_graph_data_object, parallel)
 
 
 ### get functions for update_coefficients
@@ -170,7 +106,7 @@ NDJacVecOperator10_object = NDJacVecOperator10(x, p, t, g, graph_structure_, gra
 
 ### Vertex, Edge functions for update_coefficients
 
-function VertexJacobian!(J::AbstractMatrix, v, p, t)
+function vertex_jacobian!(J::AbstractMatrix, v, p, t)
     #J = internal_jacobian(J, v, p, t)
     J[1, 1] = 1.0
     J[1, 2] = 0.0
@@ -178,7 +114,7 @@ function VertexJacobian!(J::AbstractMatrix, v, p, t)
     J[2, 2] = 0.0
 end
 
-function EdgeJacobian!(J_s::AbstractMatrix, J_d::AbstractMatrix, v_s, v_d, p, t)
+function edge_jacobian!(J_s::AbstractMatrix, J_d::AbstractMatrix, v_s, v_d, p, t)
    #J_s = source_jacobian(v_s, v_d, p, t)
    #J_d = dest_jacobian(v_s, v_d, p, t)
    J_s[1, 1] = 1.0
@@ -186,8 +122,6 @@ function EdgeJacobian!(J_s::AbstractMatrix, J_d::AbstractMatrix, v_s, v_d, p, t)
    J_d[1, 1] = 1.0
    J_d[1, 2] = 0.0
 end
-
-
 
 function update_coefficients!(Jac::NDJacVecOperator, x, p, t)
 
@@ -197,7 +131,9 @@ function update_coefficients!(Jac::NDJacVecOperator, x, p, t)
     jgd = Jac.jac_graph_data
 
     for i in 1:gs.num_v
-        vertices![i].VertexJacobian!(
+        #vertices![i].VertexJacobian!(
+        #nd_diffusion_vertex[i].VertexJacobian!(
+        nd_diffusion_vertex.vertex_jacobian!(
           get_vertex_jacobian(jgd, i),
           get_vertex(gd, i),
           p_v_idx(p, i),
@@ -205,7 +141,9 @@ function update_coefficients!(Jac::NDJacVecOperator, x, p, t)
     end
 
     for i in 1:gs.num_e
-          edges![i].EdgeJacobian!(
+          #edges![i].EdgeJacobian!(
+          #nd_diffusion[i].EdgeJacobian!(
+          nd_diffusion.edge_jacobian!(
               get_src_edge_jacobian(jgd, i),
               get_dst_edge_jacobian(jgd, i),
               get_src_vertex(gd, i),
@@ -218,6 +156,44 @@ function update_coefficients!(Jac::NDJacVecOperator, x, p, t)
     Jac.p = p
     Jac.t = t
 end
+
+# functions needed
+
+# If the type of dx and x match, we swap out v_array for x
+@inline function prep_gd(dx::AbstractArray{T}, x::AbstractArray{T}, gd::GraphData{GDB, T, T}, gs) where {GDB, T}
+    # We construct views into an Array of size dim_v, so when we swap the
+    # underlying Array it has to have to same size
+    if size(x) == (gs.dim_v,)
+         swap_v_array!(gd, x)
+         return gd
+    else
+         error("Size of x does not match the dimension of the system.")
+    end
+end
+
+# If the type of dx and x do not match, we swap initialize a new GraphData object
+# that is based on the type of dx for the edge buffer.
+# Some solvers take the derivative with respect to time, thus x will not be dual
+# but dx will be, leading to errors otherwise
+@inline function prep_gd(dx, x, gd, gs)
+    # Type mismatch
+    if size(x) == (gs.dim_v,)
+        e_array = similar(dx, gs.dim_e)
+        return GraphData(x, e_array, gs)
+    else
+        error("Size of x does not match the dimension of the system.")
+   end
+end
+
+# function call update_coefficients!
+
+x_test = randn(2*N)
+p_test = nothing
+t_test = 1.0
+
+update_coefficients!(NDJacVecOperator_object, x_test, p_test, t_test)
+
+# callable structs
 
 function (Jac::NDJacVecOperator)(x, p, t) # auch Number bei t?
     update_coefficients!(Jac, x, p, t)
@@ -235,6 +211,8 @@ function LinearAlgebra.mul!(dx::AbstractVector, Jac::NDJacVecOperator, z::Abstra
     jac_vec_prod!(dx, Jac, z)
 end
 
+# final functions for NDJacVecOperator
+
 function jac_vec_prod(Jac::NDJacVecOperator, z)
 
     gs = Jac.graph_structure
@@ -245,7 +223,7 @@ function jac_vec_prod(Jac::NDJacVecOperator, z)
     for i in 1:gs.num_e
         e_jac_product[i] .= get_src_edge_jacobian(jgd, i) * view(z, get_src_indices(i)) + get_dst_edge_jacobian(jgd, i) * view(z, get_dst_indices(i))
     end
-    ## neues array wird erstellt und returnt
+    ## neues array wird erstellt und returned
 
     dx = zeros(gs.v_dims, gs.num_v)
 
@@ -271,81 +249,3 @@ function jac_vec_prod!(dx, Jac::NDJacVecOperator, z)
         dx .+= sum(e_jac_product[i])
     end
 end
-
-
-
-### test stuff
-
-#v_jac_array = Array{Array{Float64, 2}, 1}(undef, 2)
-#v_jac_array[1] = [1.0 2.0]
-#v_jac_array[2] = [3.0 4.0]
-#println(v_jac_array)
-
-#e_jac_array = Array{Array{Array{Float64, 2}, 1}, 1}(undef, 1)
-#e_jac_array[1] = [[1.0 2.0]]
-#e_jac_array[2] = [[3.0 4.0]]
-#print(e_jac_array)
-
-#e_jac_product = Array{Float64, 2.0}
-#e_jac_product[1] = [1.0]
-
-#=mutable struct NDJacVecOperator1{uType, tType, JGD} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator1_object = NDJacVecOperator1(x, p, t, jac_graph_data_object, parallel)
-
-
-mutable struct NDJacVecOperator2{uType, tType, G, JGD} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    graph::G
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator2_object = NDJacVecOperator2(x, p, t, graph_, jac_graph_data_object, parallel)
-
-mutable struct NDJacVecOperator3{uType, tType, JGD, T} <: DiffEqBase.AbstractDiffEqLinearOperator{T} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    #graph::G
-    #graph_structure::GraphStruct
-    #graph_data::GD
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator3_object = NDJacVecOperator3(x, p, t, jac_graph_data_object, parallel)
-
-mutable struct NDJacVecOperator4{uType, tType, GD, JGD} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    #graph::G
-    #graph_structure::GraphStruct
-    graph_data::GD
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator4_object = NDJacVecOperator4(x, p, t, graph_data_, jac_graph_data_object, parallel)
-
-mutable struct NDJacVecOperator5{uType, tType, GS, GD, JGD} # mutable da x, p, t geupdated werden
-    x::uType
-    p
-    t::tType
-    #graph::G
-    graph_structure::GS
-    graph_data::GD
-    jac_graph_data::JGD
-    parallel::Bool
-end
-
-NDJacVecOperator5_object = NDJacVecOperator5(x, p, t, graph_data_, graph_structure_, jac_graph_data_object, parallel)=#
