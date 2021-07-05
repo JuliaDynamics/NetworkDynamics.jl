abf16cf3c3a2de39d392ca4c6b735888f499ccf5
Hans Würfel <git@wuerfel.io>
2021-07-05 18:56:26 +0200

    use sub environment for benchmarks

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -1,12 +1,23 @@
 #!julia
 
-# make sure PkgBenchamark is available in your standard environment
+# set root path of the ND repo
+NDPATH = begin
+    path = splitpath(abspath(@__DIR__))
+    i = findlast(isequal("NetworkDynamics"), path)
+    abspath(path[begin:i]...)
+end
+BMPATH = joinpath(NDPATH, "benchmark")
+
+# activate the benchmark folder as current environment
+import Pkg; Pkg.activate(BMPATH); Pkg.develop(path=NDPATH);
+
 using PkgBenchmark
 using LibGit2
 using Dates
 
 tstart = time()
 
+# rudimentary arg parse function
 function getarg(default, flags...)
     idx = findfirst(x -> x ∈ flags, ARGS)
     return idx===nothing ? default : ARGS[idx+1]
@@ -35,36 +46,39 @@ prefix = getarg(Dates.format(now(), "yyyy-mm-dd_HHMMSS"), "-p", "--prefix") * "_
 
 @info "Run benchmarks on $target_id and compare to $baseline_id"
 
-# set root path of the ND repo
-ndpath = begin
-    path = splitpath(abspath(@__DIR__))
-    i = findlast(isequal("NetworkDynamics"), path)
-    abspath(path[begin:i]...)
-end
-bmpath = joinpath(ndpath, "benchmark")
-
-ndpath_tmp = tempname()*"_NetworkDynamics"
+#=
+PkgBenchmark will do stuff to the repo (i.e. check out the desired branch),
+therefore we copy the repo to tmp/NetworkDynamics
+We want to have the same benchmarks for target and base therefore we copy
+NetworkDynamics/benchmark to tmp/benchmark so it won't change if the repo
+state changes.
+=#
+tmp = tempname(); mkpath(tmp)
+ndpath_tmp = joinpath(tmp, "NetworkDynamics")
 @info "Copy repo to $ndpath_tmp"
-cp(ndpath, ndpath_tmp)
-cd(ndpath_tmp)
+cp(NDPATH, ndpath_tmp)
+cd(ndpath_tmp) # the tmp repo is a good place to chill
 
-# if directory is dirty, commet everything
+# copy bm scripts to separate folder (so it won't change if the state of the nd_temp repo chagnes)
+bmpath_tmp = joinpath(tmp, "benchmark")
+cp(BMPATH, bmpath_tmp)
+script_path = joinpath(bmpath_tmp, "benchmarks.jl")
+
+# if directory is dirty, commit everything (otherwise we cant switch do a differen commit/branch)
 isdirty = with(LibGit2.isdirty, GitRepo(ndpath_tmp))
 if isdirty
     @info "Dirty directory, add everything to new commit!"
     @assert realpath(pwd()) == realpath(ndpath_tmp) "Julia is in $(pwd()) not it $ndpath_tmp"
     run(`git add -A`)
-    run(`git commit -m "tmp for benchmarking"`)
+    run(`git commit -m "tmp commit for benchmarking"`)
     # assert that repo is clean now
     with(LibGit2.isdirty, GitRepo(ndpath_tmp)) && throw(error("Repository is still dirty!"))
 end
 
-import Pkg; Pkg.activate(ndpath_tmp)
-
-# copy bmscripts to separate folder
-script_dir = tempname()*"_benchmark"
-cp(bmpath, script_dir)
-script_path = joinpath(script_dir, "benchmarks.jl")
+# activate the environment in the tmp copy of the scripts
+# env will be used for the julia processes which do the benchmarking
+Pkg.activate(bmpath_tmp)
+Pkg.develop(path=ndpath_tmp)
 
 """
 Create a runnable `Cmd` object from a string like "juila-1.5".
@@ -104,8 +118,8 @@ function benchmark(; name, id, cmd::AbstractString, retune)
                            script=script_path,
                            retune,
                            verbose)
-    exportraw && writeresults(joinpath(bmpath, prefix*name*".data"), results)
-    export_markdown(joinpath(bmpath, prefix*name*".md"), results)
+    exportraw && writeresults(joinpath(BMPATH, prefix*name*".data"), results)
+    export_markdown(joinpath(BMPATH, prefix*name*".md"), results)
     return results
 end
 
@@ -116,13 +130,13 @@ if baseline_id ∉ ["nothing", "none"]
 
     # compare the two
     judgment = judge(target, baseline)
-    export_markdown(joinpath(bmpath, prefix*"judgment.md"), judgment)
+    export_markdown(joinpath(BMPATH, prefix*"judgment.md"), judgment)
 end
 
 # copy tune file over to real repo if there is none yet or it was retuned
-if !isfile(joinpath(bmpath, "tune.json")) || retune
-    println("Update tune.json in $bmpath")
-    cp(joinpath(ndpath_tmp, "benchmark", "tune.json"), joinpath(bmpath, "tune.json"); force=retune)
+if !isfile(joinpath(BMPATH, "tune.json")) || retune
+    println("Update tune.json in $BMPATH")
+    cp(joinpath(ndpath_tmp, "benchmark", "tune.json"), joinpath(BMPATH, "tune.json"); force=retune)
 end
 
 s = round(Int, time()-tstart)
