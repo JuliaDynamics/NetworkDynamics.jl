0bdb6d450f7de0a1b84c5c6b14a4f875f2cfe75e
lindnemi <michaellindner@pik-potsdam.de>
2021-10-25 17:59:50 +0200

    simplify tests (minor)

--- test/checkbounds_test.jl
+++ test/checkbounds_test.jl
@@ -7,8 +7,6 @@ g = erdos_renyi(10,10)
 
 ov = ODEVertex(f! = (dv, v, edges, p, t) -> @inbounds( dv[1] = v[1]), dim = 1)
 se = StaticEdge(f! = (e, v_s, v_d, p, t) -> nothing, dim = 1, coupling = :undirected)
-sde = StaticDelayEdge(se)
-oe = ODEEdge(se)
 
 
 nd! = network_dynamics(ov,se,g)
