5cb7edb260c6dc611f31de2cbd32e86bb964df10
lindnemi <michaellindner@pik-potsdam.de>
2023-05-15 15:01:25 +0200

    2nd try with Power Constraints for loads

--- examples/max_dae.jl
+++ examples/max_dae.jl
@@ -21,42 +21,62 @@ function kuramoto_inertia!(dv, v, edges, P, t)
     return nothing
 end
 
-static = StaticVertex(; f=(θ, edges, c, t) -> θ[1] = c,
-                      dim=1, sym=[:θ])
+
+function load_vertex(dv, v, edges, P, t)
+    dv[1] = P
+    for edge in edges
+        dv[1] += edge[1]
+    end
+    return nothing
+end
+
+static = ODEVertex(; f=load_vertex, mass_matrix=0.0,
+                   dim=1, sym=[:θ])
 
 inertia = ODEVertex(; f=kuramoto_inertia!, dim=2, sym=[:θ, :ω])
 
 f_edge = StaticEdge(; f=kuramoto_edge!, dim=1)
 
-
-
-N = 9
+N = 8
 # Simple ring topology
 g = watts_strogatz(N, 2, 0.0)
 
-v_arr = Array{VertexFunction}([inertia for _ in 1:N-1])
-P     = shuffle!([ones((N - 1) ÷ 2); -ones((N - 1) ÷ 2)])
-x0    = vcat([[rand(Uniform(-pi, pi)), rand(Uniform(-15, 15))] for _ in 1:N-1]...)
-
-# first vertex is grid connection point
-insert!(v_arr, 1, static)
-# the microgrid is actually balanced
-insert!(x0, 1, 0.0)
-
+v_arr = shuffle!([repeat([inertia], 4); repeat([static], 4)])
+nd    = network_dynamics(v_arr, f_edge, g)
+P     = map(x -> typeof(x) == typeof(inertia) ? 1.0 : -1.0, v_arr)
+p     = (P, nothing)
 tspan = (0.0, 150.0)
-p     = ([0.0; P], nothing)
 
-nd = network_dynamics(v_arr, f_edge, g)
-# i multiplied the initial conditions with 0.1 to get convergence, for a power grid model this would be important
-# actually i don't really understand why it ended up in a desynchronized state, despite the grid connection :D
-prob = ODEProblem(nd, x0 .* 0.1, tspan, p)
+# Find an inital condition
+
+# ic_guess = rand(N * 3 ÷ 2)
+# idx_ω = idx_containing(nd, :ω)
+# ic_guess[idx_ω] .= 0.0
+# x0 = find_valid_ic(nd, rand(N * 3 ÷ 2); p=p)
+
+#fp = find_fixpoint(ndfp, p, rand(2N))
+
+ndfp = network_dynamics(inertia, f_edge, g)
+probfp = ODEProblem(ndfp, zeros(2N), tspan, p)
+solfp = solve(probfp, Tsit5())
+plot(solfp; vars=syms_containing(ndfp, :ω))
+ic = find_fixpoint(ndfp, p, zeros(2N))
+x0 = zeros(12)
+x0[idx_containg]
+
+
+
+# or try random
+x0 = find_valid_ic(nd, randn(N * 3 ÷ 2); p=p)
+
+prob = ODEProblem(nd, x0, tspan, p)
 sol = solve(prob, Rodas4())
+
 
 
 # Define node colors
 begin
-    nodefillc = [colorant"lightseagreen" for _ in 1:N-1]
-    insert!(nodefillc, 1, colorant"orange")
+    nodefillc = map(x -> typeof(x) == typeof(inertia) ? colorant"lightseagreen" : colorant"orange", v_arr)
     nodefillc = reshape(nodefillc, 1, N)
 end
 
