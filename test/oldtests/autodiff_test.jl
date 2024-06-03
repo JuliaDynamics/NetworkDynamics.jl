feeaff4ffd9038ee546ddc2656749210f063b43f
Hans Würfel <git@wuerfel.io>
2024-06-03 15:00:51 +0200

    some work on old autodiff tests

--- test/oldtests/autodiff_test.jl
+++ test/oldtests/autodiff_test.jl
@@ -10,46 +10,61 @@ g = barabasi_albert(N, k) # a little more exciting than a bare random graph
 
 @testset "Automatic Differentiation compatibility" begin
     @inline function diffusionedge!(e, v_s, v_d, p, t)
-        e[1] = p * (v_s[1] - v_d[1])
+        e[1] = p[1] * (v_s[1] - v_d[1])
         nothing
     end
 
-    @inline function diffusionvertex!(dv, v, edges, p, t)
-        dv[1] = p
-        for e in edges
-            dv[1] += e[1]
-        end
+    @inline function diffusionvertex!(dv, v, esum, p, t)
+        dv[1] = p[1] + esum[1]
         nothing
     end
     ### Constructing the network dynamics
 
-    nd_diffusion_vertex = ODEVertex(; f=diffusionvertex!, dim=1)
-    nd_diffusion_edge = StaticEdge(; f=diffusionedge!, dim=1, coupling=:antisymmetric)
+    nd_diffusion_vertex = ODEVertex(; f=diffusionvertex!, dim=1, pdim=1)
+    nd_diffusion_edge = StaticEdge(; f=diffusionedge!, dim=1, pdim=1, coupling=AntiSymmetric())
+    nd = Network(g, nd_diffusion_vertex, nd_diffusion_edge)
 
-    p_v = collect(1:nv(g)) ./ nv(g) .- 0.5
-    p_e = 0.5 .* ones(ne(g))
-    p = (p_v, p_e)
+    # p = (p_v, p_e)
+    s = NWState(nd; utype=Float64, ptype=Float64)
+    s.p.v[:,1] = collect(1:nv(g)) ./ nv(g) .- 0.5
+    s.p.e[:,1] = 0.5 .* ones(ne(g))
+    s.v[:,1] = randn(N)
 
-    nd = network_dynamics(nd_diffusion_vertex, nd_diffusion_edge, g; parallel=false)
-    x0 = randn(N)
-
-
-    ## Jacobians
-
-    @test ForwardDiff.jacobian((out, x) -> nd(out, x, p, 0.0), ones(N), ones(N)) isa Matrix
-    @test ReverseDiff.jacobian((out, x) -> nd(out, x, p, 0.0), ones(N), ones(N)) isa Matrix
+    ## Jacobians wrt. states
+    @test ForwardDiff.jacobian((out, x) -> nd(out, x, pflat(s), 0.0), ones(N), ones(N)) isa Matrix
+    @test ReverseDiff.jacobian((out, x) -> nd(out, x, pflat(s), 0.0), ones(N), ones(N)) isa Matrix
 
     ## Gradient wrt. vertex parameters
-
-    function gradpnd(p)
+    function gradpv(vp)
         # initializing the output array with the correct type is crucial
-        out = similar(p, N)
-        nd.f(out, zeros(N), (p, p_e), 0.0)
+        out = similar(vp, N)
+        _p = NWParameter(s.p, ptype=Vector{Union{eltype(vp), eltype(s.p)}})
+        _p.v[:,1] = vp
+
+        nd(out, s[:], _p[:], 0.0)
         sum(out)
     end
 
-    @test ForwardDiff.gradient(gradpnd, ones(N)) isa Vector
-    @test ReverseDiff.gradient(gradpnd, ones(N)) isa Vector
+    @test ForwardDiff.gradient(gradpv, ones(N)) isa Vector
+    @test ReverseDiff.gradient(gradpv, ones(N)) isa Vector
+    vps = rand(N)
+    @test ForwardDiff.gradient(gradpv, vps) == ReverseDiff.gradient(gradpv, vps)
+
+    ## Gradient wrt. edge parameters
+    function gradpe(ep)
+        # initializing the output array with the correct type is crucial
+        out = similar(ep, N)
+        _p = NWParameter(s.p, ptype=Vector{Union{eltype(ep), eltype(s.p)}})
+        _p.e[:,1] = ep
+
+        nd(out, s[:], _p[:], 0.0)
+        sum(out)
+    end
+
+    @test ForwardDiff.gradient(gradpe, ones(ne(g))) isa Vector
+    @test ReverseDiff.gradient(gradpe, ones(ne(g))) isa Vector
+    vps = rand(ne(g))
+    @test ForwardDiff.gradient(gradpe, vps) == ReverseDiff.gradient(gradpe, vps)
 
     ## parameter array instead of tuple
 
@@ -58,20 +73,16 @@ g = barabasi_albert(N, k) # a little more exciting than a bare random graph
         nothing
     end
 
-    @inline function diffusionvertex2!(dv, v, edges, p, t)
-        dv[1] = p[1]
-        for e in edges
-            dv[1] -= e[1]
-        end
-
+    @inline function diffusionvertex2!(dv, v, esum, p, t)
+        dv[1] = p[1] - esum[1]
         nothing
     end
-    nd_diffusion_vertex2 = ODEVertex(; f=diffusionvertex2!, dim=1)
-    nd_diffusion_edge2 = StaticEdge(; f=diffusionedge2!, dim=1, coupling=:antisymmetric)
+    nd_diffusion_vertex2 = ODEVertex(; f=diffusionvertex2!, dim=1, pdim=1)
+    nd_diffusion_edge2 = StaticEdge(; f=diffusionedge2!, dim=1, pdim=1, coupling=AntiSymmetric())
 
     p = [1.0, 2.0]
 
-    nd2 = network_dynamics(nd_diffusion_vertex2, nd_diffusion_edge2, g; parallel=false)
+    nd2 = Network(g, nd_diffusion_vertex2, nd_diffusion_edge2, g)
     nd2.f(x0, zeros(N), p, 0.0)
 
     function gradpnd2(p)
