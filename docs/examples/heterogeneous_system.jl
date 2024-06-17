8b6c8df83ab5df996612ecfcb297cb7f01818943
Hans Würfel <git@wuerfel.io>
2024-06-17 18:31:12 +0200

    adapt heterogenous example

--- docs/examples/heterogeneous_system.jl
+++ docs/examples/heterogeneous_system.jl
@@ -12,7 +12,6 @@ We start by setting up a simple system of Kuramoto oscillators.
 
 using NetworkDynamics, OrdinaryDiffEq, Plots, Graphs
 
-
 N = 8
 g = watts_strogatz(N, 2, 0) # ring network
 
@@ -26,31 +25,41 @@ function kuramoto_vertex!(dθ, θ, esum, (ω,), t)
     nothing
 end
 
-vertex! = ODEVertex(kuramoto_vertex!; sym=[:θ], psym=[:ω])
-edge!   = StaticEdge(kuramoto_edge!; dim=1, psym=[:K], coupling=AntiSymmetric())
+vertex! = ODEVertex(kuramoto_vertex!; sym=[:θ], psym=[:ω], name=:kuramoto)
+
+edge! = StaticEdge(kuramoto_edge!; dim=1, psym=[:K=>3], coupling=AntiSymmetric())
 nw = Network(g, vertex!, edge!);
 nothing #hide
 
 #=
-Introducing heterogeneous parameters is as easy as defining an array.
-Here the vertex parameters are heterogeneous, while the edges share the same coupling
-parameter K.
+To assign parameters, we can create a `NWParameter` object based on the `nw` definition.
+This parameter object will be pre-filled with the default parameters.
 =#
+p = NWParameter(nw)
 
+#=
+To set the node parameters, we can use indexing of the `p.v` field:
+=#
 ω = collect(1:N) ./ N
 ω .-= sum(ω) / N
-K = 3.0
-p = (ω, K); # p[1] vertex parameters, p[2] edge parameters
+p.v[:, :ω] = ω
 nothing #hide
 
 #=
-Integrate and plot
-=#
+Here, the index pairing `:, :ω` is used to index state ω for all node indices.
 
+The parameter object contains information about the network structure. For the
+actual problem definition we need to throw away this wrapper and use the
+flat-vector representation of the parameters `pflat(p)`. Note that `pflat(p)`
+
+Similarily, we could use `NWState(nw)` to create an indexable wrapper of the
+initial state. However in this case we can also fill create the flat state
+array manually:
+=#
 x0 = collect(1:N) ./ N
 x0 .-= sum(x0) ./ N
 tspan = (0.0, 10.0)
-prob = ODEProblem(nw, x0, tspan, p)
+prob = ODEProblem(nw, x0, tspan, pflat(p))
 sol = solve(prob, Tsit5())
 plot(sol; ylabel="θ", fmt=:png)
 
@@ -58,78 +67,104 @@ plot(sol; ylabel="θ", fmt=:png)
 ## Heterogeneous dynamics
 
 Two paradigmatic modifications of the node model above are static nodes and nodes with
-inertia. A static node has no internal dynamics and instead fixes the variable at a
-constant value. A Kuramoto model with inertia consists of two internal variables leading to
-more complicated (and for many applications more realistic) local dynamics.
+inertia.
+
+A static node has no internal dynamics and instead fixes the variable at a
+constant value. There are two ways to achive such behavior in network dynamics.
+We can either create a [`StaticVertex`](@ref) which is implemented as an
+algebraic constraint, or we can create a node without dynamic (du = 0) and a
+specific initial value.
 =#
 
-static! = StaticVertex(; f=(θ, edges, c, t) -> θ .= c, dim=1, sym=[:θ])
-
-
-function kuramoto_inertia!(dv, v, edges, P, t)
-    dv[1] = v[2]
-    dv[2] = P - 1.0 * v[2]
-    for e in edges
-        dv[2] += e[1]
-    end
+static! = ODEVertex(sym=[:θ=>ω[1]], name=:static) do du, u, esum, p, t
+    du .= 0
+    nothing
 end
 
-inertia! = ODEVertex(; f=kuramoto_inertia!, dim=2, sym=[:θ, :ω]);
+#=
+Here we used a form of the ODEVertex constructor which allows us to specify
+default initial conditions.
+
+A Kuramoto model with inertia consists of two internal variables leading to
+more complicated (and for many applications more realistic) local dynamics.
+=#
+function kuramoto_inertia!(dv, v, esum, (ω,), t)
+    dv[1] = v[2]
+    dv[2] = ω - 1.0 * v[2] + esum[1]
+    nothing
+end
+
+inertia! = ODEVertex(kuramoto_inertia!; sym=[:θ, :ω], psym=[:ω], name=:inertia)
 nothing #hide
 
 #=
 Since now we model a system with heterogeneous node dynamics we can no longer
-straightforwardly pass a single VertexFunction to `network_dynamics` but instead have to
-hand over an Array.
+straightforwardly pass a single VertexFunction to the `Network` constructor but
+instead have to hand over an Array.
 =#
 
-vertex_array    = Array{VertexFunction}([vertex! for i in 1:N])
+vertex_array    = ODEVertex[vertex! for i in 1:N]
 vertex_array[1] = static!
 vertex_array[5] = inertia! # index should correspond to the node's index in the graph
-nd_hetero! = network_dynamics(vertex_array, edge!, g);
-nothing #hide
+nw_hetero! = Network(g, vertex_array, edge!)
 
 #=
 Now we have to take a bit more care with defining initial conditions and parameters.
-For the static! node the initial condition has to match its parameter. For simplicity
-we use the same parameters as above.
+
+First, we can generate a `NWState` object based on the `nw_hetero!` object which
+will be populated with the default values.
 =#
 
-x0[1] = ω[1];
-nothing #hide
+state = NWState(nw_hetero!)
 
 #=
 The node with inertia is two-dimensional, hence we need to specify two initial conditions.
 For the first dimension we keep the initial conditions from above and insert! another one into `x0` at
 the correct index.
-=#
 
-inertia_ic_2 = 5
-insert!(x0, 6, inertia_ic_2);
+For the θ states we will use the same initial conditins as before:
+=#
+state.v[2:8,:θ] = x0[2:8]
 nothing #hide
 
 #=
-`x0[1:4]` holds ic for nodes 1 to 4, `x0[5:6]` holds the two
-initial conditions for node 5, `x0[7:9]` holds initial conditions for nodes 6 to 8.
+We're still missing one initial condition: the second variable ω of the 5th vertex.
 =#
-
-prob_hetero = ODEProblem(nd_hetero!, x0, tspan, p)
-sol_hetero = solve(prob_hetero, Rodas4());
+state.v[5,:ω] = 5
 nothing #hide
+
+#=
+The `NWState` object also contains a parameter object accessible via `state.p`.
+The edge parameters are already filled with default values.
+The vertex parameters can be copied from our old parmeter object `p`.
+=#
+state.p.v[2:8, :ω] = p.v[2:8, :ω]
+nothing #hide
+
+#=
+For the problem construction, we need to convert the nested stuctures to flat arrays using the [`uflat`](@ref) and [`pflat`](@ref) methods.
+=#
+prob_hetero = ODEProblem(nw_hetero!, uflat(state), tspan, pflat(state))
+sol_hetero = solve(prob_hetero, Tsit5());
+nothing #hide
+plot(sol_hetero)
 
 #=
 For clarity we plot only the variables referring to the oscillator's angle θ and color
 them according to their type.
 =#
 
-membership = ones(Int64, N)
-membership[1] = 2
-membership[5] = 3
-nodecolor = [colorant"lightseagreen", colorant"orange", colorant"darkred"];
-nodefillc = reshape(nodecolor[membership], 1, N);
+colors = map(vertex_array) do vertexf
+    if vertexf.name == :kuramoto
+        colorant"lightseagreen"
+    elseif vertexf.name == :static
+        colorant"orange"
+    elseif vertexf.name == :inertia
+        colorant"darkred"
+    end
+end
 
-vars = syms_containing(nd_hetero!, :θ);
-plot(sol_hetero; ylabel="θ", vars=vars, lc=nodefillc, fmt=:png)
+plot(sol_hetero; ylabel="θ", idxs=vidxs(1:8,:θ), lc=colors', fmt=:png)
 
 #=
 ## Components with algebraic constraints
