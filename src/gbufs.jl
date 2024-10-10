8fb151f7c002600e6a2e12b28920256876ec3da8
Hans Würfel <git@wuerfel.io>
2024-10-10 19:46:19 +0200

    add propagate_inbounds annotation

--- src/gbufs.jl
+++ src/gbufs.jl
@@ -12,26 +12,9 @@ end
 
 function EagerGBufProvider(im::IndexManager, batches)
     map = zeros(Int, ne(im.g) * im.vdepth, 2)
-    for batch in batches
-        for i in 1:length(batch)
-            eidx = batch.indices[i]
-            e = im.edgevec[eidx]
-            dst_range = im.v_data[e.src][1:im.vdepth]
-            src_range = im.v_data[e.dst][1:im.vdepth]
-            range = gbuf_range(batch, i)
-            map[range, 1] .= dst_range
-            map[range, 2] .= src_range
-        end
-    end
-
-    map2 = zeros(Int, ne(im.g) * im.vdepth, 2)
     for i in 1:ne(im.g)
-        map2[im.e_gbufr[i], 1] .= im.e_src[i]
-        map2[im.e_gbufr[i], 2] .= im.e_dst[i]
-    end
-
-    if map != map2
-        error("nope not the same")
+        map[im.e_gbufr[i], 1] .= im.e_src[i]
+        map[im.e_gbufr[i], 2] .= im.e_dst[i]
     end
 
     N = ForwardDiff.pickchunksize(max(im.lastidx_dynamic, im.lastidx_p))
@@ -44,7 +27,7 @@ function get_gbuf(bufp::EagerGBufProvider, u)
     gbuf
 end
 
-@inline function get_src_dst(gbuf::AbstractArray, batch, i)
+Base.@propagate_inbounds function get_src_dst(gbuf::AbstractArray, batch, i)
     bufr = @views gbuf_range(batch, i)
     src = @views gbuf[bufr, 1]
     dst = @views gbuf[bufr, 2]
@@ -70,9 +53,10 @@ function get_gbuf(bufp::LazyGBufProvider, u)
     LazyGBuf(bufp, u)
 end
 
-@inline function get_src_dst(gbuf::LazyGBuf, batch, i)
-    eidx = @views batch.indices[i]
-    src = @views gbuf.u[gbuf.lbp.e_src[eidx]]
-    dst = @views gbuf.u[gbuf.lbp.e_dst[eidx]]
+Base.@propagate_inbounds function get_src_dst(gbuf::LazyGBuf, batch, i)
+    eidx =  batch.indices[i]
+    lbp = gbuf.lbp
+    src = view(gbuf.u, lbp.e_src[eidx])
+    dst = view(gbuf.u, lbp.e_dst[eidx])
     src, dst
 end
