6d867511b75ff40a44bbc7b179eb4555d94141dc
Hans Würfel <git@wuerfel.io>
2025-02-17 08:42:32 +0100

    allow for multi=false in multiselect

--- NetworkDynamicsInspector/test/multiselect_test.jl
+++ NetworkDynamicsInspector/test/multiselect_test.jl
@@ -9,7 +9,8 @@ gui = (;
         :foo,
         :bar
     ]),
-    selection = Observable{Vector{Symbol}}(Symbol[:Rust]),
+    selection = Observable{Vector{Symbol}}(Symbol[:Rust, :French]),
+    selection_single = Observable{Vector{Symbol}}(Symbol[:Julia])
 )
 
 SERVER = Ref{Any}()
@@ -17,12 +18,10 @@ let
     app = App(;) do session
         NetworkDynamicsInspector.clear_obs!(gui)
 
-        ms = MultiSelect(gui.options, gui.selection; placeholder="pick language", T=Symbol)
-        @info "compare" ms.selection gui.selection ms.selection===gui.selection
+        ms1 = MultiSelect(gui.options, gui.selection; placeholder="pick language", T=Symbol)
+        ms2 = MultiSelect(gui.options, gui.selection_single; placeholder="pick language", multi=false, T=Symbol)
 
-        return DOM.div(
-            ms
-        )
+        return Grid(ms1, ms2)
     end;
     try
         println("Close existing")
@@ -34,10 +33,30 @@ let
     # Bonito.update_app!
 end
 
+@test gui.selection[] == [:Rust, :French]
 gui.selection[] = [:Rust, :French, :Julia, :foo]
+gui.selection[] = []
 
+@test gui.selection_single[] == [:Julia]
+gui.selection_single[] = [:Rust]
+gui.selection_single[] = [:French, :Julia]
+@test gui.selection_single[] == [:French]
+gui.selection_single[] = []
+
+gui.selection[] = [:Rust, :French]
+gui.selection_single[] = [:French]
+# change options
 gui.options[] = [
         OptionGroup("Programming Languages", [:Julia, :Rust, :Java]),
     ]
+@test gui.selection[] == [:Rust]
+@test gui.selection_single[] == []
+
+# TODO
+# - css file for app
+
+
+
+#
 
 gui.selection[] = [:Julia, :Rust]
