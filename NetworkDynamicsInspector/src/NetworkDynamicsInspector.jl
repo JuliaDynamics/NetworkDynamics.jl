23fd287ee2604883953c60ea34e5b0c976c4c0ea
Hans Würfel <git@wuerfel.io>
2025-02-26 21:27:06 +0100

    add methods for manipulating appstate

--- NetworkDynamicsInspector/src/NetworkDynamicsInspector.jl
+++ NetworkDynamicsInspector/src/NetworkDynamicsInspector.jl
@@ -25,7 +25,7 @@ using ColorSchemes: ColorSchemes, ColorScheme
 using SciMLBase: SciMLBase
 
 # defined based on the julia version
-using NetworkDynamics: AnnotatedIOBuffer, AnnotatedString
+using NetworkDynamics: AnnotatedIOBuffer, AnnotatedString, @styled_str
 
 include("utils.jl")
 
@@ -43,7 +43,8 @@ include("timeseries.jl")
 
 const SymbolicCompIndex = Union{VIndex{Int,Nothing}, EIndex{Int,Nothing}}
 
-export inspect
+export inspect, dump_app_state
+export set_sol!, set_state!, set_graphplot!, set_timeseries!, define_timeseries!
 
 @kwdef struct GraphPlot
     nstate::Observable{Vector{Symbol}} = [:nothing]
@@ -240,6 +241,140 @@ function inspect(sol; restart=false, reset=false)
     else
         @info "App still served at $(SERVER[].url):$(SERVER[].port)"
     end
+    nothing
+end
+
+function appstate()
+    isnothing(APPSTATE[]) && error("Uninitialized appstate. To initialize call `set_sol!(sol)` or `inspect(sol)`!")
+    APPSTATE[]
+end
+
+# helper constrcut to mark undefined keyword arguments
+struct NotSpecified end
+function set_maybe!(obs::Observable, val)
+    if obs[] != val
+        obs[] = val
+    end
+end
+set_maybe!(obs::Observable, ::NotSpecified) = nothing
+
+"""
+    set_sol!(sol)
+
+Set the solution of the current appstate to `sol`.
+"""
+function set_sol!(sol)
+    if isnothing(APPSTATE[])
+        APPSTATE[] = AppState(sol)
+    else
+        APPSTATE[].sol[] = sol
+    end
+    nothing
+end
+
+"""
+    set_state!(; sol, t, tmin, tmax)
+
+Set the solution, current time and time limits of the current appstate.
+
+To automaticially create commands see [`dump_app_state()`](@ref).
+"""
+function set_state!(; sol = NotSpecified(),
+                      t = NotSpecified(),
+                      tmin = NotSpecified(),
+                      tmax = NotSpecified())
+    sol != NotSpecified() && set_sol!(sol)
+    set_maybe!(appstate().t, t)
+    set_maybe!(appstate().tmin, tmin)
+    set_maybe!(appstate().tmax, tmax)
+    nothing
+end
+
+"""
+    set_graphplot!(; nstate, estate, nstate_rel, estate_rel, ncolorrange, ecolorrange)
+
+Set the properties of the graphplot of the current appstate.
+
+To automaticially create commands see [`dump_app_state()`](@ref).
+"""
+function set_graphplot!(; nstate = NotSpecified(),
+                          estate = NotSpecified(),
+                          nstate_rel = NotSpecified(),
+                          estate_rel = NotSpecified(),
+                          ncolorrange = NotSpecified(),
+                          ecolorrange = NotSpecified())
+    gp = appstate().graphplot
+    set_maybe!(gp.nstate, nstate)
+    set_maybe!(gp.estate, estate)
+    set_maybe!(gp.nstate_rel, nstate_rel)
+    set_maybe!(gp.estate_rel, estate_rel)
+    set_maybe!(gp.ncolorrange, ncolorrange)
+    set_maybe!(gp.ecolorrange, ecolorrange)
+    nothing
+end
+
+"""
+    set_timeseries!(key; selcomp, states, rel)
+
+Set properties of the timeseries plot with key `key`. See also [`define_timeseries!`](@ref).
+
+To automaticially create commands see [`dump_app_state()`](@ref).
+"""
+function set_timeseries!(key; selcomp = NotSpecified(),
+                              states = NotSpecified(),
+                              rel = NotSpecified())
+    if !haskey(appstate().tsplots, key)
+        appstate().tsplots[key] = TimeseriesPlot()
+    end
+    tsplot = appstate().tsplots[key]
+    set_maybe!(tsplot.selcomp, selcomp)
+    set_maybe!(tsplot.states, states)
+    set_maybe!(tsplot.rel, rel)
+    nothing
+end
+
+"""
+    define_timeseries!(tsarray)
+
+Defines timeseries, where `tsarray` is an array of timeseries keyword arguments
+(see [`set_timeseries!`](@ref)).
+
+To automaticially create commands see [`dump_app_state()`](@ref).
+"""
+function define_timeseries!(tsarray)
+    if length(tsarray) != length(appstate().tsplots[])
+        @warn "Due to current limitations, you need to reload the page if the number of timeseries plots changes"
+        empty!(appstate().tsplots[])
+        tskeys = [gendomid("ts") for _ in tsarray]
+    else
+        tskeys = keys(appstate().tsplots[])
+    end
+    for (key, ts) in zip(tskeys, tsarray)
+        set_timeseries!(key; pop!(tsargs)...)
+    end
+    nothing
+end
+
+"""
+    dump_app_state()
+
+Generate a list of [`set_sol!`](@ref), [`set_state!`](@ref), [`set_graphplot!`](@ref) and [`define_timeseries!`](@ref)
+commands to recreate the current appstate.
+The intended usecase is to quickly recreate "starting points" for interactive exploration.
+"""
+function dump_app_state()
+    appstate()
+    println("To recreate the current state, run the following commands:\n")
+    println(styled"set_sol!({red:sol}) # optional if after inspect(sol)")
+    println("set_state!(; t=$(appstate().t[]), tmin=$(appstate().tmin[]), tmax=$(appstate().tmax[]))")
+    gp = appstate().graphplot
+    println("set_graphplot!(; nstate=$(gp.nstate[]), estate=$(gp.estate[]), nstate_rel=$(gp.nstate_rel[]), estate_rel=$(gp.estate_rel[]), ncolorrange=$(gp.ncolorrange[]), ecolorrange=$(gp.ecolorrange[]))")
+    println("define_timeseries!([")
+    for ts in values(appstate().tsplots[])
+        selstr = replace(repr(ts.selcomp[]), r"^.*\["=>"[")
+        println("    (; selcomp=$(selstr), states=$(ts.states[]), rel=$(ts.rel[])),")
+    end
+    println("])")
     nothing
 end
 
