664291ac5d26ae6a701c84f010b46359d502ec74
Hans Würfel <git@wuerfel.io>
2025-02-27 17:00:15 +0100

    fix tests

--- NetworkDynamicsInspector/test/runtests.jl
+++ NetworkDynamicsInspector/test/runtests.jl
@@ -6,11 +6,14 @@ using Aqua
 
 @testset "Package Quality Tests" begin
     @test check_no_implicit_imports(NetworkDynamicsInspector) === nothing
-
     @test check_no_stale_explicit_imports(NetworkDynamicsInspector) === nothing
+
     Aqua.test_all(NetworkDynamicsInspector;
         ambiguities=false,
         stale_deps=true,
-        persistent_tasks=false)
+        persistent_tasks=false,
+        piracies = (;treat_as_own = [NetworkDynamics.extract_nw])
+    )
+
     @test_broken isempty(Docs.undocumented_names(NetworkDynamicsInspector))
 end
