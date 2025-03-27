1f1de32d5cd54855b1898ab3c316c58bfe30ad0b
Hans Würfel <git@wuerfel.io>
2025-03-27 12:07:26 +0100

    update import functions

--- test/importexport_test.jl
+++ test/importexport_test.jl
@@ -1,6 +1,7 @@
 using NetworkDynamics
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as Dt
+using StableRNGs
 
 @mtkmodel Swing begin
     @variables begin
@@ -35,8 +36,8 @@ end
 end
 
 @mtkmodel StaticPowerLine begin
-    @structural_parameters begin
-        K = 1.63 # structural parameter to keep them out of nw parameters
+    @parameters begin
+        K = 1.63, [description = "line conductance parameter"]
     end
     @variables begin
         srcθ(t), [description = "voltage angle at src end", input=true]
@@ -55,29 +56,59 @@ function CombinedModel(subsystems...; kwargs...)
         θ(t) = 0.0, [description = "voltage angle", output=true]
         Pel(t), [description = "Electical power flowing from network into node", input=true]
     end
-    θ_eqs   = [θ ~ sub.θ for sub in subsystes]
-    Pel_eqs = [Pel ~ sub.Pel for sub in subsystems]
-    ODESystem(; kwargs...)
+    eqs = [
+        (θ ~ sub.θ for sub in subsystems)...,
+        Pel ~ +((sub.Pel for sub in subsystems)...) # kirchoff constaint
+    ]
+    ODESystem(eqs, t, [θ, Pel], []; systems=collect(subsystems), kwargs...)
 end
 
-mtk_models = [
-    Swing(Pmech=1, M=1, D=1.0; name=:G1),
-    Load(Pload; name=:L2),
-    Swing(Pmech=1, M=1, D=1.0; name=:G2),
-    Load(Pload; name=:L3),
-    Swing(Pmech=1, M=1, D=1.0; name=:G3),
-    Load(Pload; name=:L4),
-];
+v1 = let
+    @named swing = Swing(; Pmech=1.0, M=1.0, D=1.0)
+    VertexModel(swing, [:Pel], [:θ], vidx=1)
+end
 
-vertexmodels = map(mtk_models) do mtk_model
-    VertexModel(mtk_model, [:Pel], [:θ])
-end;
-vertexmodels2 = map(mtk_models2) do mtk_model
-    VertexModel(mtk_model, [:Pel], [:θ])
-end;
+v2 = let
+    @named load = Load(; Pd=-1.0)
+    VertexModel(load, [:Pel], [:θ], vidx=2)
+end
 
+v3 = let
+    @named swing = Swing(; Pmech=1.0, M=1.0, D=1.0)
+    @named load = Load(; Pd=-2.0)
+    @named swingload = CombinedModel(swing, load)
+    VertexModel(swingload, [:Pel], [:θ], vidx=3)
+end
+
+v4 = let
+    @named swing = Swing(; Pmech=2.0, M=1.0, D=1.0)
+    @named load = Load(; Pd=-1.0)
+    @named swingload2 = CombinedModel(swing, load)
+    VertexModel(swingload2, [:Pel], [:θ], vidx=4)
+end
+
+e1 = let
+    @named line = StaticPowerLine(; K=0.5)
+    EdgeModel(line, [:srcθ], [:dstθ], [:P]; annotation=:AntiSymmetric, src=1, dst=2)
+end
+e2 = let
+    @named line = StaticPowerLine(; K=1.0)
+    EdgeModel(line, [:srcθ], [:dstθ], [:P]; annotation=:AntiSymmetric, src=2, dst=3)
+end
+e3 = let
+    @named line = StaticPowerLine()
+    EdgeModel(line, [:srcθ], [:dstθ], [:P]; annotation=:AntiSymmetric, src=3, dst=4)
+end
+
+nw = Network([v1,v2,v3,v4],[e1,e2,e3])
+
+rng = StableRNG(1)
+u = randn(rng, dim(nw))
+du1 = [NaN for _ in 1:dim(nw)]
+nw(du1, u, pflat(NWState(nw)), NaN)
 
 data = NetworkDynamics.parse_network("testgrid.yaml")
+data["VertexModels"][1]
 
 constructors = Dict("Swing"=>Swing, "VertexModel"=>VertexModel, "Load"=>Load)
 NetworkDynamics.build_network(data, constructors)
