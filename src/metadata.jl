a02235f5f0d6057bedd75f916bdf9b1762a0bd54
Hans Würfel <git@wuerfel.io>
2025-04-07 12:37:22 +0200

    fix signature

--- src/metadata.jl
+++ src/metadata.jl
@@ -309,7 +309,7 @@ Removes the component-wide metadata `key` from the component model,
 or from a component referenced by `idx` in a network.
 Returns `true` if the metadata existed and was removed, `false` otherwise.
 """
-function delete_metadata!(c::ComponentModel, key::Symbol, val)
+function delete_metadata!(c::ComponentModel, key::Symbol)
     md = metadata(c)
     if haskey(md, key)
         delete!(md, key)
