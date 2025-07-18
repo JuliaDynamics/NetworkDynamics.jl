6373990e57e96154e5b91c7caaaefe0eaf73f4fc
Documenter.jl <documenter@juliadocs.github.io>
2025-07-18 18:14:14 +0000

    build based on 0dbcd93

--- previews/PR253/generated/init_tutorial.jl
+++ previews/PR253/generated/init_tutorial.jl
@@ -247,6 +247,11 @@ du = ones(dim(nw_dyn))
 nw_dyn(du, uflat(u0_dyn), pflat(u0_dyn), 0.0)
 extrema(du .- zeros(dim(nw_dyn))) # very close to zero, confirming we have a steady state!
 
+# Alternatively, we can used the `isfixpoint` function to check if the state is a fixpoint:
+
+@assert isfixpoint(nw_dyn, u0_dyn)
+nothing #hide
+
 # ## Simulating the Dynamic Model
 #
 # With our properly initialized model, we can now simulate the system to observe its behavior.
