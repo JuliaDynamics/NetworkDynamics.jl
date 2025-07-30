aac1f65c36c713593bcefb1ff579230f47676041
Hans Würfel <git@wuerfel.io>
2025-07-30 10:29:53 +0200

    update localmake

--- docs/localmake.jl
+++ docs/localmake.jl
@@ -10,20 +10,18 @@ At the end of each run the user is prompted to rerun the make process. Using rev
 use the updated `*.md` and source files. This way the Julia session keeps alive and the
 individual builds are much faster.
 =#
-
-#=
 using Pkg
+using Revise
+using LiveServer
+
 Pkg.activate(@__DIR__)
-Pkg.instantiate()
+Pkg.develop(PackageSpec(path=dirname(@__DIR__))) # adds the package this script is called from
 print("Do you want to update docs environment? [y/n] ")
 answer = readline()
 if !isempty(answer) && answer[1] == 'y'
     Pkg.update()
 end
-
-using Revise
-using LiveServer
-using REPL.TerminalMenus
+Pkg.instantiate()
 
 port = isempty(ARGS) ? 8000 : parse(Int, ARGS[1])
 @assert 8000 ≤ port ≤ 9000 "port has to be in range 8000..9000!"
@@ -31,8 +29,8 @@ port = isempty(ARGS) ? 8000 : parse(Int, ARGS[1])
 @info "Start server..."
 @async serve(;dir=joinpath(@__DIR__, "build"), port)
 
-menu = RadioMenu(["Run again!", "Quit!"])
-while true
+run = true
+while run
     revise()
     @info "Start building docs..."
     try
@@ -41,23 +39,22 @@ while true
         @info "make.jl error" e
     end
 
-    println("\nDocs are served at http://localhost:$port")
-
-    if request("What now?", menu) != 1
-        break
+    printstyled("\n\nDocs are served at http://localhost:$port\n\n", color=:blue, bold=true)
+    println("Run again? Enter! Exit with 'q'.")
+    if readline() == "q"
+        global run = false
     end
 end
-=#
 
 
-using Pkg
-Pkg.activate(@__DIR__)
-using NetworkDynamics
-using LiveServer
+# using Pkg
+# Pkg.activate(@__DIR__)
+# using NetworkDynamics
+# using LiveServer
 
-cd(pkgdir(NetworkDynamics))
+# cd(pkgdir(NetworkDynamics))
 
-servedocs(
-    literate_dir = joinpath("docs", "examples"),
-    skip_dir = joinpath("docs", "src", "generated")
-)
+# servedocs(
+#     literate_dir = joinpath("docs", "examples"),
+#     skip_dir = joinpath("docs", "src", "generated")
+# )
