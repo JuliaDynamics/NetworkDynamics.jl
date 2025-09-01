732fff83fc824bbb067f52a794afdb28d21768ea
Hans Würfel <git@wuerfel.io>
2025-09-01 15:10:36 +0200

    remove stale imports

--- src/NetworkDynamics.jl
+++ src/NetworkDynamics.jl
@@ -83,9 +83,8 @@ export @initformula, InitFormula
 include("init_constraints.jl")
 
 using OrderedCollections: OrderedDict
-using NonlinearSolve: AbstractNonlinearSolveAlgorithm, NonlinearFunction
-using NonlinearSolve: NonlinearLeastSquaresProblem, NonlinearProblem
-using SteadyStateDiffEq: SteadyStateProblem, SteadyStateDiffEqAlgorithm, SSRootfind
+using NonlinearSolve: NonlinearFunction, NonlinearLeastSquaresProblem
+using SteadyStateDiffEq: SteadyStateProblem, SSRootfind
 export find_fixpoint, set_interface_defaults!
 export initialize_component, initialize_component!, init_residual
 export initialize_componentwise, initialize_componentwise!, interface_values
