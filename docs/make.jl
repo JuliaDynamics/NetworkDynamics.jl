87e4e78dd81e833ed3109874c8bdbbf9ff8517d0
Hans Würfel <git@wuerfel.io>
2026-03-05 13:51:50 +0100

    avoid unnecessary rebuild of docs

--- docs/make.jl
+++ docs/make.jl
@@ -38,7 +38,8 @@ end
 mtkext = Base.get_extension(NetworkDynamics, :NetworkDynamicsMTKExt)
 dfext = Base.get_extension(NetworkDynamics, :NetworkDynamicsDataFramesExt)
 sparsityext = Base.get_extension(NetworkDynamics, :NetworkDynamicsSparsityExt)
-kwargs = (;
+
+doc = makedocs(;
     root=joinpath(pkgdir(NetworkDynamics), "docs"),
     sitename="NetworkDynamics",
     modules=[NetworkDynamics, mtkext, dfext, sparsityext, NetworkDynamicsInspector],
@@ -74,28 +75,17 @@ kwargs = (;
     ],
     draft=haskey(ENV, "DOCUMENTER_DRAFT"),
     format = Documenter.HTML(ansicolor = true, size_threshold=1_000_000, assets=String["assets/custom.css"]),
-    warnonly=[:missing_docs],
+    warnonly=true,
+    debug=true, # return doc object
 )
-kwargs_warnonly = (; kwargs..., warnonly=true)
 
-if haskey(ENV,"GITHUB_ACTIONS")
-    success = true
-    thrown_ex = nothing
-    try
-        makedocs(; kwargs...)
-    catch e
-        @info "Strict doc build failed, try again with warnonly=true"
-        global success = false
-        global thrown_ex = e
-        makedocs(; kwargs_warnonly...)
-    end
-
+if haskey(ENV, "GITHUB_ACTIONS")
     deploydocs(; repo="github.com/JuliaDynamics/NetworkDynamics.jl.git",
             devbranch="main", push_preview=true)
-
-    success || throw(thrown_ex)
-else # local build
-    makedocs(; kwargs_warnonly...)
+    errors = setdiff(doc.internal.errors, [:missing_docs])
+    if !isempty(errors)
+        error("makedocs encountered errors: $(errors)")
+    end
 end
 
 @info "Close global electron window"
